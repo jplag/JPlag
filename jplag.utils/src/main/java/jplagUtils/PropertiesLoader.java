5cfe5979063fdb3634df9c2b59fedf1b603beba3
Mathias Landhäußer <mathias.landhaeusser@kit.edu>
2015-03-27 19:59:49 +0100

    Use try-with-resources when loading properties

--- jplag.utils/src/main/java/jplagUtils/PropertiesLoader.java
+++ jplag.utils/src/main/java/jplagUtils/PropertiesLoader.java
@@ -6,15 +6,13 @@ import java.util.Properties;
 
 public class PropertiesLoader {
 	public static Properties loadProps(String resourceName) {
-		InputStream in = PropertiesLoader.class.getClassLoader().getResourceAsStream(resourceName);
 		Properties props = new Properties();
-		if (in != null) {
-			try {
-				props.load(in);
-				in.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		try (InputStream in = PropertiesLoader.class.getClassLoader()
+				.getResourceAsStream(resourceName);) {
+			props.load(in);
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		return props;
 	}
