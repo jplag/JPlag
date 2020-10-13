2218aa8b068a822659f7b42abdda719e507f57e8
Philipp Bauch <ga53xex@mytum.de>
2020-10-13 18:54:21 +0200

    Pass JPlagOptions to Program constructor

--- jplag/src/main/java/jplag/JPlag.java
+++ jplag/src/main/java/jplag/JPlag.java
@@ -1,24 +1,21 @@
 package jplag;
 
-
-import jplag.options.CommandLineOptions;
+import jplag.options.JPlagOptions;
 
 public class JPlag {
-	public static void main(String[] args) {
-		if (args.length == 0)
-			CommandLineOptions.usage();
-		else {
-            try {
-                CommandLineOptions options = new CommandLineOptions(args, null);
-                Program program = new Program(options);
 
-                System.out.println("initialize ok");
-                program.run();
-            }
-            catch(ExitException ex) {
-                System.out.println("Error: "+ex.getReport());
-                System.exit(1);
-            }
-		}
-	}
+  public static void main(String[] args) {
+
+    try {
+      JPlagOptions options = JPlagOptions.fromArgs(args);
+      Program program = new Program(options);
+
+      System.out.println("JPlag initialized");
+      program.run();
+    } catch (ExitException ex) {
+      System.out.println("Error: " + ex.getReport());
+      System.exit(1);
+    }
+    
+  }
 }
--- jplag/src/main/java/jplag/options/JPlagOptions.java
+++ jplag/src/main/java/jplag/options/JPlagOptions.java
@@ -1,2 +1,20 @@
-package jplag.options;public class JPlagOptions {
+package jplag.options;
+
+public class JPlagOptions {
+
+  private Language language;
+
+  private Verbosity verbosity;
+
+  public static JPlagOptions fromArgs(String[] args) {
+    return new JPlagOptions();
+  }
+
+  public Language getLanguage() {
+    return language;
+  }
+
+  public Verbosity getVerbosity() {
+    return verbosity;
+  }
 }
