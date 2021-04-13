85164a6ca9603f0a5bd1ef800c2ef74184dd412c
Philipp Bauch <philippbauch@outlook.com>
2021-04-13 23:38:58 +0200

    Add argparse4j library

--- jplag/src/main/java/jplag/CLI.java
+++ jplag/src/main/java/jplag/CLI.java
@@ -3,10 +3,23 @@ package jplag;
 import java.io.File;
 import jplag.options.LanguageOption;
 import jplag.reporting.Report;
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
 
 public class CLI {
 
   public static void main(String[] args) {
+    ArgumentParser parser = ArgumentParsers.newFor("prog").build()
+        .description("Process some integers.");
+    parser.addArgument("integers")
+        .metavar("N")
+        .type(Integer.class)
+        .nargs("+")
+        .help("an integer for the accumulator");
+    parser.addArgument("--sum")
+        .dest("accumulate")
+        .help("sum the integers (default: find the max)");
+
     try {
       JPlagOptions options = new JPlagOptions(
           "/Users/philippbauch/Develop/jplag-test",
