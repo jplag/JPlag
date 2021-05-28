9dda8ccdcc1549314f19b5d66cb5831835182186
Timur Sağlam <timur.saglam@kit.edu>
2021-05-28 12:15:39 +0200

    Formatted language and parser classes.

--- jplag.frontend.java-1.9/src/main/java/jplag/java19/Language.java
+++ jplag.frontend.java-1.9/src/main/java/jplag/java19/Language.java
@@ -6,65 +6,63 @@ import jplag.ProgramI;
 import jplag.Structure;
 
 /**
- * Hello world!
- *
+ * Language for Java 9 and newer.
  */
 public class Language implements jplag.Language {
-	private Parser parser;
+    private Parser parser;
 
-	public Language(ProgramI program) {
-		this.parser = new Parser();
-		this.parser.setProgram(program);
+    public Language(ProgramI program) {
+        this.parser = new Parser();
+        this.parser.setProgram(program);
+    }
 
-	}
-	
     public String[] suffixes() {
-    	String[] res = { ".java", ".JAVA" };
-		return res;
-	}
+        String[] res = {".java", ".JAVA"};
+        return res;
+    }
 
-	public String name() {
-		return "Javac 1.9+ based AST plugin";
-	}
+    public String name() {
+        return "Javac 1.9+ based AST plugin";
+    }
 
-	public String getShortName() {
-		return "java19";
-	}
+    public String getShortName() {
+        return "java19";
+    }
 
-	public int min_token_match() {
-		return 9;
-	}
-	public boolean supportsColumns() {
-		return true;
-	}
+    public int min_token_match() {
+        return 9;
+    }
 
-	public boolean isPreformatted() {
-		return true;
-	}
+    public boolean supportsColumns() {
+        return true;
+    }
 
-	public boolean usesIndex() {
-		return false;
-	}
+    public boolean isPreformatted() {
+        return true;
+    }
 
-	public int noOfTokens() {
-		return JavaToken.numberOfTokens();
-	}
+    public boolean usesIndex() {
+        return false;
+    }
 
-	public String type2string(int type) {
-		return JavaToken.type2string(type);
-	}
+    public int noOfTokens() {
+        return JavaToken.numberOfTokens();
+    }
 
+    public String type2string(int type) {
+        return JavaToken.type2string(type);
+    }
 
-	public Structure parse(File dir, String[] files) {
-		return this.parser.parse(dir, files);
-	}
+    public Structure parse(File dir, String[] files) {
+        return this.parser.parse(dir, files);
+    }
 
-	public boolean errors() {
-		return this.parser.getErrors();
-	}
+    public boolean errors() {
+        return this.parser.getErrors();
+    }
 
-	public int errorsCount() {
-		return this.parser.errorsCount();
-	}
+    public int errorsCount() {
+        return this.parser.errorsCount();
+    }
 
 }
