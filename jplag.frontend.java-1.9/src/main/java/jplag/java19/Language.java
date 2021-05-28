5afd1f15f7eb524a2c4d8d16728da25923ade790
Timur Sağlam <timur.saglam@kit.edu>
2021-05-28 12:17:17 +0200

    Made implicit overrides explicit.

--- jplag.frontend.java-1.9/src/main/java/jplag/java19/Language.java
+++ jplag.frontend.java-1.9/src/main/java/jplag/java19/Language.java
@@ -16,51 +16,63 @@ public class Language implements jplag.Language {
         this.parser.setProgram(program);
     }
 
+    @Override
     public String[] suffixes() {
         String[] res = {".java", ".JAVA"};
         return res;
     }
 
+    @Override
     public String name() {
         return "Javac 1.9+ based AST plugin";
     }
 
+    @Override
     public String getShortName() {
         return "java19";
     }
 
+    @Override
     public int min_token_match() {
         return 9;
     }
 
+    @Override
     public boolean supportsColumns() {
         return true;
     }
 
+    @Override
     public boolean isPreformatted() {
         return true;
     }
 
+    @Override
     public boolean usesIndex() {
         return false;
     }
 
+    @Override
     public int noOfTokens() {
         return JavaToken.numberOfTokens();
     }
 
+    @Override
     public String type2string(int type) {
         return JavaToken.type2string(type);
     }
 
+    @Override
     public Structure parse(File dir, String[] files) {
         return this.parser.parse(dir, files);
     }
 
+    @Override
     public boolean errors() {
         return this.parser.getErrors();
     }
 
+    @Override
     public int errorsCount() {
         return this.parser.errorsCount();
     }
