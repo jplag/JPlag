700e9cc4924af35c47d4d9c6ad3c64e1922de34e
Timur Sağlam <timur.saglam@kit.edu>
2021-06-25 09:43:13 +0200

    Applied auto-formatting to the frontend-utils.

--- jplag.frontend-utils/src/main/java/jplag/Parser.java
+++ jplag.frontend-utils/src/main/java/jplag/Parser.java
@@ -5,27 +5,27 @@ package jplag;
  * @date 22.01.2005
  */
 public abstract class Parser { // TODO TS: We should rename this class, as all concrete parsers shadow its name.
-  protected ProgramI program;
-  protected int errors    = 0;
-  private   int errorsSum = 0;
-  
-  public boolean getErrors() {
-	  return (errors!=0);
-  }
-  
-  public int errorsCount() {
-	  return errorsSum;	
-  }
-  
-  protected void parseEnd() {
-	  errorsSum+=errors;	
-  }
+    protected ProgramI program;
+    protected int errors = 0;
+    private int errorsSum = 0;
 
-  public ProgramI getProgram() {
-	  return program;
-  }
-  
-  public void setProgram(ProgramI prog) {
-	  this.program=prog;
-  }
+    public boolean getErrors() {
+        return (errors != 0);
+    }
+
+    public int errorsCount() {
+        return errorsSum;
+    }
+
+    protected void parseEnd() {
+        errorsSum += errors;
+    }
+
+    public ProgramI getProgram() {
+        return program;
+    }
+
+    public void setProgram(ProgramI prog) {
+        this.program = prog;
+    }
 }
