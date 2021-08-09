4278454adff3fa55e637edba4b1c660092d755a6
Timur Sağlam <timur.saglam@kit.edu>
2021-08-09 13:57:52 +0200

    Add submission tuple class.

---
+++ jplag/src/main/java/jplag/strategy/SubmissionTuple.java
@@ -0,0 +1,30 @@
+package jplag.strategy;
+
+import jplag.Submission;
+
+/**
+ * Tupel of source code submissions.
+ */
+public class SubmissionTuple {
+    private final Submission left;
+    private final Submission right;
+
+    /**
+     * Creates a tupel from two submissions.
+     */
+    public SubmissionTuple(Submission left, Submission right) {
+        if (left == null || right == null) {
+            throw new IllegalArgumentException("Submissions cannot be null");
+        }
+        this.left = left;
+        this.right = right;
+    }
+
+    public Submission getLeft() {
+        return left;
+    }
+
+    public Submission getRight() {
+        return right;
+    }
+}
