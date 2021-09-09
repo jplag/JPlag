5ec95fcde0859b84e7908e2338d90f6087019b7e
Timur Sağlam <timur.saglam@kit.edu>
2021-09-09 13:25:08 +0200

    Add test cases for the CLI options base code and min token match.

---
+++ jplag/src/test/java/jplag/cli/BaseCodeOptionTest.java
@@ -0,0 +1,26 @@
+package jplag.cli;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+import jplag.CommandLineArgument;
+
+public class BaseCodeOptionTest extends CommandLineInterfaceTest {
+
+    private static final String NAME = "BaseCodeName";
+
+    @Test
+    public void testDefaultValue() {
+        buildOptionsFromCLI(CURRENT_DIRECTORY);
+        assertNull(options.getBaseCodeSubmissionName());
+    }
+
+    @Test
+    public void testCustomName() {
+        String argument = buildArgument(CommandLineArgument.BASE_CODE, NAME);
+        buildOptionsFromCLI(argument, CURRENT_DIRECTORY);
+        assertEquals(NAME, options.getBaseCodeSubmissionName());
+    }
+}
