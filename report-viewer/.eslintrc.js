f46c7f6349b9f61dfc98e01a40ce47443a216822
Evgeni <evan.ch97@gmail.com>
2021-11-24 17:48:48 +0200

    [192-webreport] Disabled prettier/prettier linting in eslint.js

--- report-viewer/.eslintrc.js
+++ report-viewer/.eslintrc.js
@@ -16,5 +16,6 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'prettier/prettier': 0,
   },
 };
