6256e702593bf3e543fdad9fa8cdf25657370a14
Evgeni <evan.ch97@gmail.com>
2021-11-24 17:47:51 +0200

    [192-webreport] Deleted auto generated from vue project creator code and file

--- report-viewer/src/App.vue
+++ report-viewer/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+
   </div>
   <router-view />
 </template>
@@ -16,15 +15,14 @@
 }
 
 #nav {
-  padding: 30px;
+
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+
 }
 </style>
