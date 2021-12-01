03d9d994a5cb085f7615d6ee2d8bac8d7792c85a
Timur Sağlam <timur.saglam@kit.edu>
2021-12-01 11:24:21 +0100

    Clean-up char frontend.

--- jplag.frontend.chars/src/main/java/de/jplag/chars/CharToken.java
+++ jplag.frontend.chars/src/main/java/de/jplag/chars/CharToken.java
@@ -3,30 +3,36 @@ package de.jplag.chars;
 import de.jplag.Token;
 
 public class CharToken extends Token {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private int index;
+    private int index;
 
-	public CharToken(int type, String file, int index, Parser parser) {
-		super(type, file, -1);
-		this.index = index;
-	}
+    public CharToken(int type, String file, int index, Parser parser) {
+        super(type, file, -1);
+        this.index = index;
+    }
 
-	public CharToken(int type, String file, Parser parser) {
-		this(type, file, -1, parser);
-	}
+    public CharToken(int type, String file, Parser parser) {
+        this(type, file, -1, parser);
+    }
 
-	@Override
+    @Override
     public int getLine() {
-		return index; // so that the result-index is sorted correctly
-	}
+        return index; // so that the result-index is sorted correctly
+    }
 
-	@Override
-    public int getColumn() { return 0; }
+    @Override
+    public int getColumn() {
+        return 0;
+    }
 
-	@Override
-    public int getLength() { return 0; }
+    @Override
+    public int getLength() {
+        return 0;
+    }
 
-	@Override
-    public int getIndex() { return index; }
+    @Override
+    public int getIndex() {
+        return index;
+    }
 }
