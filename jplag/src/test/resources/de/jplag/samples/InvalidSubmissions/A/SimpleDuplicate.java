da8b67cdf6c3a06b44a818b0812987b5040a4e51
Timur Sağlam <timur.saglam@kit.edu>
2021-12-17 11:21:06 +0100

    Add test case for the debug option.

---
+++ jplag/src/test/resources/de/jplag/samples/InvalidSubmissions/A/SimpleDuplicate.java
@@ -0,0 +1,11 @@
+public class SimpleDuplicate {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!")
+
+        for(int i = 0; i < 10; i++) {
+            System.out.println("Number is " + i);
+        }
+    }
+
+}
