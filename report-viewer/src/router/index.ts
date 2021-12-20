db93c15abc88329e759998a765caa9daad6282c5
Evgeni <evan.ch97@gmail.com>
2021-12-20 13:46:47 +0200

    [192-webreport] Removed FileUpload

--- report-viewer/src/router/index.ts
+++ report-viewer/src/router/index.ts
@@ -1,19 +1,12 @@
-import {createRouter, createWebHashHistory, createWebHistory, RouteRecordRaw} from "vue-router";
+import {createRouter, createWebHistory, RouteRecordRaw} from "vue-router";
 import Overview from "@/views/Overview.vue";
-import FileUpload from "@/views/FileUpload.vue"
 import ComparisonView from "@/views/ComparisonView.vue"
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/f",
-    name: "FileUpload",
-    component: FileUpload
-  },
-  {
     path: "/",
     name: "Overview",
     component: Overview,
-    //props: route => ({ jsonString : route.params.str })
   },
   {
     path: "/comparison/:id1/:id2",
