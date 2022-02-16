6acab021fe45d67ec3b88c3f99a0596e0109ff5f
Simon Ding <me@simonding.de>
2022-02-16 14:26:13 +0100

    JDoc comment for CligroupHelper

--- jplag/src/main/java/de/jplag/CliGroupHelper.java
+++ jplag/src/main/java/de/jplag/CliGroupHelper.java
@@ -8,6 +8,10 @@ import net.sourceforge.argparse4j.inf.ArgumentGroup;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.MutuallyExclusiveGroup;
 
+/**
+ * Can be used to automatically create and reuse {@link ArgumentGroup}s and {@link MutuallyExclusiveGroup}s through
+ * their names only. This is useful when an {@link ArgumentParser} is not configured in an imperative fashion.
+ */
 public class CliGroupHelper {
 
     private final ArgumentParser parser;
