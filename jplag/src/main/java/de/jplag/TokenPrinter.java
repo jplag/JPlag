98c420512bde085f0a592481f6971d6d495db678
Timur Sağlam <timur.saglam@kit.edu>
2022-02-22 10:36:02 +0100

    Build an initial version of a token printer that visualizes JPlag tokens.

---
+++ jplag/src/main/java/de/jplag/TokenPrinter.java
@@ -0,0 +1,154 @@
+package de.jplag;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Utility class for printing JPlag tokens from a submission.
+ * @see Submission
+ * @author Timur Saglam
+ */
+public final class TokenPrinter {
+
+    // Representation:
+    private static final String BAR = "|";
+    private static final String TAB = "\t";
+    private static final String SPACE = " ";
+    private static final String TAB_REPLACEMENT = "        "; // might depend on files
+
+    // Configuration:
+    private static final boolean INDICATE_TINY_TOKEN = true; // print token with length <= 0 in lowercase
+    private static final boolean REPLACE_TABS = false;
+
+    /**
+     * Creates a string representation of a submission line by line and adds the tokens under the lines.
+     * @param submission is the submission to print.
+     * @return the string representation.
+     */
+    public static String printTokens(Submission submission) {
+        return printTokens(submission.getTokenList(), submission.getFiles());
+    }
+
+    /**
+     * Creates a string representation of a set of files line by line and adds the tokens under the lines.
+     * @param tokens is the set of tokens parsed from the files.
+     * @param directory is the common directory of the files.
+     * @param fileNames is a collection of the file names.
+     * @return the string representation.
+     */
+    public static String printTokens(TokenList tokens, File directory, Collection<String> fileNames) {
+        Collection<File> files = fileNames.stream().map(name -> new File(directory, name)).collect(toList());
+        return printTokens(tokens, files);
+    }
+
+    /**
+     * Creates a string representation of a collection of files line by line and adds the tokens under the lines.
+     * @param tokens is the set of tokens parsed from the files.
+     * @param files are the parsed files.
+     * @return the string representation.
+     */
+    public static String printTokens(TokenList tokens, Collection<File> files) {
+        Map<String, List<String>> filesToLines = readFiles(files);
+        StringBuilder builder = new StringBuilder();
+        int lineIndex = 0;
+        int columnIndex = 1;
+        String file = null; // no file yet
+
+        for (Token token : tokens.allTokens()) {
+            // New code file:
+            if (!token.getFile().equals(file)) {
+                if (file != null) {
+                    builder.append(System.lineSeparator());
+                }
+                file = token.getFile();
+                builder.append(file);
+                lineIndex = 0;
+            }
+
+            // New code line:
+            if (token.getLine() > lineIndex) {
+                lineIndex = token.getLine();
+                columnIndex = 1;
+                String currentLine = filesToLines.get(token.getFile()).get(lineIndex - 1);
+                if (REPLACE_TABS) {
+                    currentLine = currentLine.replace(TAB, TAB_REPLACEMENT);
+                }
+                appendCodeLinePrefix(builder, lineIndex).append(currentLine);
+                appendTokenLinePrefix(builder);
+            }
+
+            // New line if already past token index:
+            if (columnIndex > token.getColumn()) {
+                appendTokenLinePrefix(builder);
+                columnIndex = 1;
+            }
+            // Move to token index:
+            while (columnIndex < token.getColumn()) {
+                builder.append(SPACE);
+                columnIndex++;
+            }
+
+            // Print the actual token:
+            String stringRepresentation = getStringRepresentation(token);
+            builder.append(BAR).append(stringRepresentation);
+            columnIndex += stringRepresentation.length() + 1;
+
+            // Move up to token end:
+            int tokenEndIndex = token.getColumn() + token.getLength() - 1;
+            while (columnIndex < tokenEndIndex) {
+                builder.append(SPACE);
+                columnIndex++;
+            }
+            // Print token end if not already past it:
+            if (columnIndex == tokenEndIndex) {
+                builder.append(BAR);
+                columnIndex++;
+            }
+        }
+        return builder.toString();
+
+    }
+
+    private static StringBuilder appendCodeLinePrefix(StringBuilder builder, int index) {
+        return builder.append(System.lineSeparator()).append(index + TAB);
+    }
+
+    private static StringBuilder appendTokenLinePrefix(StringBuilder builder) {
+        return builder.append(System.lineSeparator()).append(TAB);
+    }
+
+    /**
+     * Determines the string representation of the token.
+     */
+    private static String getStringRepresentation(Token token) {
+        return token.getLength() <= 1 && INDICATE_TINY_TOKEN ? token.toString().toLowerCase() : token.toString();
+    }
+
+    /**
+     * Turns a list of files into a map which maps the file names to a list of the contained lines of that file.
+     */
+    private static Map<String, List<String>> readFiles(Collection<File> allFiles) {
+        return allFiles.stream().collect(toMap(it -> it.getName(), it -> linesFromFile(it)));
+    }
+
+    /**
+     * Parses a file and returns a list of the contained lines.
+     */
+    private static List<String> linesFromFile(File file) {
+        try {
+            return Files.readAllLines(file.toPath());
+        } catch (IOException exception) {
+            System.err.println("Cannot read " + file.getAbsolutePath() + ":");
+            exception.printStackTrace();
+        }
+        return Collections.emptyList();
+    }
+}
