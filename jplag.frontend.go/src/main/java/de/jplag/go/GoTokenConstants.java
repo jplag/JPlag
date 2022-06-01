b9b3f0cbe01077bc9bdfc6bbbb9795e374c9f014
Robin Maisch <robinmaisch95@gmail.com>
2022-06-01 17:31:46 +0200

    Reformat GoTokenConstants

--- jplag.frontend.go/src/main/java/de/jplag/go/GoTokenConstants.java
+++ jplag.frontend.go/src/main/java/de/jplag/go/GoTokenConstants.java
@@ -4,46 +4,60 @@ import de.jplag.TokenConstants;
 
 public interface GoTokenConstants extends TokenConstants {
 
-    enum TokenType {
-        FILE_END,
-        SEPARATOR_TOKEN,
-        STRUCT_DECLARATION
-    }
-
     // Top level structures
-    int STRUCT_DECLARATION_BEGIN = 2, STRUCT_BODY_BEGIN = 3, STRUCT_BODY_END = 4,
 
-            MEMBER_DECLARATION = 5,
+    int STRUCT_DECLARATION_BEGIN = 2;
+    int STRUCT_BODY_BEGIN = 3;
+    int STRUCT_BODY_END = 4;
+    int MEMBER_DECLARATION = 5;
+    int FUNCTION_DECLARATION = 6;
+    int METHOD_DECLARATION = 7;
+    int FUNCTION_PARAMETER = 8;
+    int FUNCTION_BODY_BEGIN = 9;
+    int FUNCTION_BODY_END = 10;
 
-            FUNCTION_DECLARATION = 6, METHOD_DECLARATION = 7, FUNCTION_PARAMETER = 8, FUNCTION_BODY_BEGIN = 9, FUNCTION_BODY_END = 10,
+    // Control flow statements
 
-            // Control flow statements
+    int IF_STATEMENT = 11;
+    int IF_BLOCK_BEGIN = 12;
+    int IF_BLOCK_END = 13;
+    int ELSE_BLOCK_BEGIN = 14;
+    int ELSE_BLOCK_END = 15;
+    int FOR_STATEMENT = 16;
+    int FOR_BLOCK_BEGIN = 17;
+    int FOR_BLOCK_END = 18;
+    int SWITCH_STATEMENT = 19;
+    int SWITCH_BLOCK_BEGIN = 20;
+    int SWITCH_BLOCK_END = 21;
+    int SWITCH_CASE = 22;
+    int CASE_BLOCK_BEGIN = 23;
+    int CASE_BLOCK_END = 24;
 
-            IF_STATEMENT = 11,
+    // Statements
 
-            IF_BLOCK_BEGIN = 12, IF_BLOCK_END = 13, ELSE_BLOCK_BEGIN = 14, ELSE_BLOCK_END = 15,
+    int FUNCTION_LITERAL = 25;
+    int ASSIGNMENT = 26;
+    int INVOCATION = 27;
+    int ARGUMENT = 28;
+    int STATEMENT_BLOCK_BEGIN = 29;
+    int STATEMENT_BLOCK_END = 30;
 
-            FOR_STATEMENT = 16, FOR_BLOCK_BEGIN = 17, FOR_BLOCK_END = 18,
+    // Object Creation
 
-            SWITCH_STATEMENT = 19, SWITCH_BLOCK_BEGIN = 20, SWITCH_BLOCK_END = 21,
+    int STRUCT_CONSTRUCTOR = 31;
+    int STRUCT_VALUE = 32;
+    int ARRAY_CONSTRUCTOR = 33;
+    int SLICE_CONSTRUCTOR = 34;
+    int MAP_CONSTRUCTOR = 35;
 
-            SWITCH_CASE = 22, CASE_BLOCK_BEGIN = 23, CASE_BLOCK_END = 24,
+    // Control Flow Keywords
 
-            // Statements
-
-            FUNCTION_LITERAL = 25,
-
-            ASSIGNMENT = 26, INVOCATION = 27, ARGUMENT = 28, STATEMENT_BLOCK_BEGIN = 29, STATEMENT_BLOCK_END = 30,
-
-            // Object Creation
-            STRUCT_CONSTRUCTOR = 31, STRUCT_VALUE = 32,
-
-            ARRAY_CONSTRUCTOR = 33, SLICE_CONSTRUCTOR = 34, MAP_CONSTRUCTOR = 35,
-
-            // Control Flow Keywords
-
-            RETURN = 36, BREAK = 37, CONTINUE = 38, GOTO = 39,
-            GO = 40, DEFER = 41;
+    int RETURN = 36;
+    int BREAK = 37;
+    int CONTINUE = 38;
+    int GOTO = 39;
+    int GO = 40;
+    int DEFER = 41;
 
     int NUM_DIFF_TOKENS = 42;
 
