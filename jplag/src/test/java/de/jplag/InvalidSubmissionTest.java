55c2ce4ff0c8647fb7b0dfc337a093a05fc2bfe5
Dominik Fuchß <develop@fuchss.org>
2022-06-25 11:42:15 +0200

    Recreate old test case

--- jplag/src/test/java/de/jplag/InvalidSubmissionTest.java
+++ jplag/src/test/java/de/jplag/InvalidSubmissionTest.java
@@ -6,9 +6,9 @@ import java.io.File;
 import java.nio.file.Path;
 import java.util.Arrays;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import de.jplag.exceptions.ExitException;
 import de.jplag.exceptions.SubmissionException;
 
 class InvalidSubmissionTest extends TestBase {
@@ -20,15 +20,21 @@ class InvalidSubmissionTest extends TestBase {
      * invalid submissions being stored.
      */
     @Test
-    void testInvalidSubmissionsWithDebug() {
-        Assertions.assertThrows(SubmissionException.class, () -> runJPlag(SAMPLE_NAME, it -> it.setDebugParser(true)));
-
-        File errorFolder = new File(Path.of(BASE_PATH, SAMPLE_NAME, "errors", "java").toString());
-        assertTrue(errorFolder.exists());
-        String[] errorSubmissions = errorFolder.list();
-        Arrays.sort(errorSubmissions); // File systems don't promise alphabetical order.
-        deleteDirectory(errorFolder.getParentFile());
-        assertArrayEquals(new String[] {"A", "B"}, errorSubmissions);
+    void testInvalidSubmissionsWithDebug() throws ExitException {
+        try {
+            runJPlag(SAMPLE_NAME, it -> it.setDebugParser(true));
+            fail("No submission exception was thrown!");
+        } catch (SubmissionException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            File errorFolder = new File(Path.of(BASE_PATH, SAMPLE_NAME, "errors", "java").toString());
+            assertTrue(errorFolder.exists());
+            String[] errorSubmissions = errorFolder.list();
+            if (errorSubmissions != null)
+                Arrays.sort(errorSubmissions); // File systems don't promise alphabetical order.
+            deleteDirectory(errorFolder.getParentFile());
+            assertArrayEquals(new String[] {"A", "B"}, errorSubmissions);
+        }
     }
 
     private static void deleteDirectory(File path) {
