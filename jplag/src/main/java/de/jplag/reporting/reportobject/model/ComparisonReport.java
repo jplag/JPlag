419daea58880cd365e74ac94f4f4c599de9576d9
Nesta Bentum <nesta.bentum@gmail.com>
2022-07-30 15:45:34 +0200

    Reformat

--- jplag/src/main/java/de/jplag/reporting/reportobject/model/ComparisonReport.java
+++ jplag/src/main/java/de/jplag/reporting/reportobject/model/ComparisonReport.java
@@ -11,8 +11,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  * @param matchPercentage similarity in percent. between 0f and 100f.
  * @param matches the list of matches found in the comparison of the two submissions
  */
-public record ComparisonReport(@JsonProperty("id1") String firstSubmissionId,
-        @JsonProperty("id2") String secondSubmissionId, @JsonProperty("similarity") float matchPercentage,
-        @JsonProperty("matches") List<Match> matches) {
+public record ComparisonReport(@JsonProperty("id1") String firstSubmissionId, @JsonProperty("id2") String secondSubmissionId,
+        @JsonProperty("similarity") float matchPercentage, @JsonProperty("matches") List<Match> matches) {
 
 }
--- jplag/src/main/java/de/jplag/reporting/reportobject/model/Match.java
+++ jplag/src/main/java/de/jplag/reporting/reportobject/model/Match.java
@@ -3,6 +3,6 @@ package de.jplag.reporting.reportobject.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record Match(@JsonProperty("file1") String firstFileName, @JsonProperty("file2") String secondFileName,
-        @JsonProperty("start1") int startInFirst, @JsonProperty("end1") int endInFirst,
-        @JsonProperty("start2") int startInSecond, @JsonProperty("end2") int endInSecond, @JsonProperty("tokens") int tokens) {
+        @JsonProperty("start1") int startInFirst, @JsonProperty("end1") int endInFirst, @JsonProperty("start2") int startInSecond,
+        @JsonProperty("end2") int endInSecond, @JsonProperty("tokens") int tokens) {
 }
