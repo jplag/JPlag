c73f4d2c8295bb25cff6ae0dfce1a0c30595654d
Robin Maisch <robinmaisch95@gmail.com>
2022-08-01 12:05:03 +0200

    Implement feedback from revision; remove duplicate dependency

--- jplag.frontend.golang/src/main/java/de/jplag/golang/Language.java
+++ jplag.frontend.golang/src/main/java/de/jplag/golang/Language.java
@@ -7,8 +7,8 @@ import de.jplag.TokenList;
 
 public class Language implements de.jplag.Language {
 
-    private static final String SHORT_NAME = "Go";
     private static final String NAME = "Go Parser";
+    private static final String SHORT_NAME = "Go";
     private static final int DEFAULT_MIN_TOKEN_MATCH = 8;
     private static final String[] FILE_EXTENSIONS = {".go"};
     private final GoParserAdapter parserAdapter;
--- jplag.frontend.golang/src/main/java/de/jplag/golang/grammar/GoTokenUtils.java
+++ jplag.frontend.golang/src/main/java/de/jplag/golang/grammar/GoTokenUtils.java
@@ -5,17 +5,18 @@ import de.jplag.golang.GoToken;
 public class GoTokenUtils {
 
     private static final int NOT_SET = -1;
-    private static final String NOT_SET_STRING = "";
+    private static final String EMPTY_STRING = "";
+
+    private GoTokenUtils() {
+        // Utility class, should not be instantiated.
+    }
 
     public static GoToken getDummyToken(int type) {
-        return new GoToken(type, NOT_SET_STRING, NOT_SET, NOT_SET, NOT_SET);
+        return new GoToken(type, EMPTY_STRING, NOT_SET, NOT_SET, NOT_SET);
     }
 
     public static GoToken getDummyToken(int type, String fileName) {
         return new GoToken(type, fileName, NOT_SET, NOT_SET, NOT_SET);
     }
 
-    private GoTokenUtils() {
-        // Utility class, should not be instantiated.
-    }
 }
