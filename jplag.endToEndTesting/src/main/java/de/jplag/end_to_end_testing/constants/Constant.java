479d73de8f1efcbf9321e9db4bd0d4fb7edeb1ac
Patrick Thiel <ucgbq@student.kit.edu>
2022-08-07 00:22:31 -0700

    adjusted temporary folder.

--- jplag.endToEndTesting/src/main/java/de/jplag/end_to_end_testing/constants/Constant.java
+++ jplag.endToEndTesting/src/main/java/de/jplag/end_to_end_testing/constants/Constant.java
@@ -14,17 +14,12 @@ public final class Constant {
     /**
      * Name for the folder to copy the submissions to in order to test them with JPlag
      */
-    public static final String TEMPORARY_DIRECTORY_NAME = "submission";
-    /**
-     * Identifier to find the system specific temporary directory.
-     */
-    private static final String TEMPORARY_SYSTEM_DIRECTORY = "java.io.tmpdir";
+    public static final String TEMPORARY_DIRECTORY_NAME = "testing-directory-submission";
     /**
      * Create the complete path to the submission files. Here the temporary system path is extended with the
      * "TEMPORARY_DIRECTORY_NAME", which is predefined in this class.
      */
-    public static final String TEMPORARY_SUBMISSION_DIRECTORY_NAME = Path.of(System.getProperty(TEMPORARY_SYSTEM_DIRECTORY), TEMPORARY_DIRECTORY_NAME)
-            .toString();
+    public static final String TEMPORARY_SUBMISSION_DIRECTORY_NAME = Path.of("target", TEMPORARY_DIRECTORY_NAME).toString();
 
     /**
      * Base path to the created plagiarism and the main file located in the project resources.
