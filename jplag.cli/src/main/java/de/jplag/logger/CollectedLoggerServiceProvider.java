9910d1509cf4b830ac35d9f1f17282a8486ee3c0
Dominik Fuchß <dominik.fuchss@kit.edu>
2022-08-24 14:30:50 +0200

    Update CLI Logging for SLF4j 2.0

---
+++ jplag.cli/src/main/java/de/jplag/logger/CollectedLoggerServiceProvider.java
@@ -0,0 +1,44 @@
+package de.jplag.logger;
+
+import org.kohsuke.MetaInfServices;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.IMarkerFactory;
+import org.slf4j.helpers.BasicMarkerFactory;
+import org.slf4j.helpers.NOPMDCAdapter;
+import org.slf4j.spi.MDCAdapter;
+import org.slf4j.spi.SLF4JServiceProvider;
+
+@MetaInfServices(SLF4JServiceProvider.class)
+public class CollectedLoggerServiceProvider implements SLF4JServiceProvider {
+    public static String REQUESTED_API_VERSION = "2.0.99";
+    private ILoggerFactory loggerFactory;
+    private IMarkerFactory markerFactory;
+    private MDCAdapter mdcAdapter;
+
+    @Override
+    public ILoggerFactory getLoggerFactory() {
+        return loggerFactory;
+    }
+
+    @Override
+    public IMarkerFactory getMarkerFactory() {
+        return markerFactory;
+    }
+
+    @Override
+    public MDCAdapter getMDCAdapter() {
+        return mdcAdapter;
+    }
+
+    @Override
+    public String getRequestedApiVersion() {
+        return REQUESTED_API_VERSION;
+    }
+
+    @Override
+    public void initialize() {
+        this.loggerFactory = new CollectedLoggerFactory();
+        this.markerFactory = new BasicMarkerFactory();
+        this.mdcAdapter = new NOPMDCAdapter();
+    }
+}
