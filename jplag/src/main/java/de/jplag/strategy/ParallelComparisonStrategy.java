acb2e56d6b3cc99e520a46aaa0c0752ed2ad7696
Jan Wittler <jan.wittler@kit.edu>
2022-09-15 12:36:46 +0200

    simplify parallel comparison strategy by using parallel streams

--- jplag/src/main/java/de/jplag/strategy/ParallelComparisonStrategy.java
+++ jplag/src/main/java/de/jplag/strategy/ParallelComparisonStrategy.java
@@ -1,8 +1,7 @@
 package de.jplag.strategy;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.*;
 
 import org.slf4j.Logger;
@@ -32,51 +31,12 @@ public class ParallelComparisonStrategy extends AbstractComparisonStrategy {
         if (withBaseCode) {
             compareSubmissionsToBaseCode(submissionSet);
         }
-        ExecutorService threadPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
-        // Parallel compare:
-        List<Submission> submissions = submissionSet.getSubmissions();
-        List<SubmissionTuple> tuples = buildComparisonTuples(submissions);
-        Collections.shuffle(tuples); // Reduce how often cache collisions occur
-        List<JPlagComparison> comparisons = Collections.synchronizedList(new ArrayList<>());
-        for (SubmissionTuple tuple : tuples) {
-            threadPool.execute(compareTuple(tuple, comparisons));
-        }
+        List<SubmissionTuple> tuples = buildComparisonTuples(submissionSet.getSubmissions());
+        List<JPlagComparison> comparisons = tuples.stream().parallel().map(tuple -> compareSubmissions(tuple.left(), tuple.right()))
+                .flatMap(Optional::stream).toList();
 
-        // Clean up and return result:
-        shutdownThreadPool(threadPool);
         long durationInMillis = System.currentTimeMillis() - timeBeforeStartInMillis;
         return new JPlagResult(comparisons, submissionSet, durationInMillis, options);
     }
-
-    /**
-     * Creates a runnable which compares a submission tuple.
-     * @param tuple contains the submissions to compare.
-     * @param comparisons is the list to append the result to.
-     * @return the runnable for parallel use.
-     */
-    private Runnable compareTuple(SubmissionTuple tuple, List<JPlagComparison> comparisons) {
-        return new Runnable() {
-            @Override
-            public void run() {
-                compareSubmissions(tuple.left(), tuple.right()).ifPresent(comparisons::add);
-            }
-        };
-    }
-
-    /**
-     * Shuts down the thread pool and awaits termination
-     */
-    private void shutdownThreadPool(ExecutorService threadPool) {
-        threadPool.shutdown();
-        try {
-            if (!threadPool.awaitTermination(TIMEOUT_IN_SECONDS, TimeUnit.SECONDS)) {
-                throw new IllegalStateException("Parallel comparison calculation timed out!");
-            }
-        } catch (InterruptedException e) {
-            logger.error(e.getMessage(), e);
-            Thread.currentThread().interrupt();
-        }
-    }
-
 }
