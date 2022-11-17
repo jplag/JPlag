b89e04483111fc889633f5f8633e64640f0f0ca8
Dominik Fuchß <develop@fuchss.org>
2022-11-17 19:04:39 +0100

    Add check that Version can be loaded correctly.

--- core/src/test/java/de/jplag/reporting/reportobject/ReportObjectFactoryTest.java
+++ core/src/test/java/de/jplag/reporting/reportobject/ReportObjectFactoryTest.java
@@ -1,5 +1,7 @@
 package de.jplag.reporting.reportobject;
 
+import de.jplag.reporting.reportobject.model.Version;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -7,5 +9,6 @@ class ReportObjectFactoryTest {
     @Test
     void testVersion() {
         Assertions.assertNotNull(ReportObjectFactory.REPORT_VIEWER_VERSION);
+        Assertions.assertNotEquals(Version.DEVELOPMENT, ReportObjectFactory.REPORT_VIEWER_VERSION);
     }
 }
