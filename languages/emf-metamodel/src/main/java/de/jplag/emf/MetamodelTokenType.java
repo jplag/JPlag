f0db68cba794162c26acb6528ae914ccd63dba4b
Timur Sağlam <timur.saglam@kit.edu>
2022-12-05 12:14:56 +0100

    De-duplicate metamodel token descriptions and add documentation.

--- languages/emf-metamodel/src/main/java/de/jplag/emf/MetamodelTokenType.java
+++ languages/emf-metamodel/src/main/java/de/jplag/emf/MetamodelTokenType.java
@@ -7,26 +7,26 @@ import de.jplag.TokenType;
  */
 public enum MetamodelTokenType implements TokenType {
     PACKAGE("EPackage"),
-    PACKAGE_END("EPackage", true),
+    PACKAGE_END(PACKAGE),
     ANNOTATION("EAnnotation"),
     CLASS("EClass"),
-    CLASS_END("EClass", true),
+    CLASS_END(CLASS),
     DATATYPE("EDatatype"),
     ENUM("EEnum"),
-    ENUM_END("EEnum", true),
+    ENUM_END(ENUM),
     ENUM_LITERAL("EEnumLiteral"),
     OPERATION("EOperation"),
-    OPERATION_END("EOperation", true),
+    OPERATION_END(OPERATION),
     REFERENCE("EReference"),
     ATTRIBUTE("EAttribute"),
     PARAMETER("EParameter"),
     INTERFACE("EInterface"),
-    INTERFACE_END("EInterface", true),
+    INTERFACE_END(INTERFACE),
     SUPER_TYPE("ESuperType"),
     ID_ATTRIBUTE("EAttribute (ID)"),
     CONTAINMENT("EReference (Containment)"),
     ABSTRACT_CLASS("EAbstractClass"),
-    ABSTRACT_CLASS_END("EAbstractClass", true),
+    ABSTRACT_CLASS_END(ABSTRACT_CLASS),
     RETURN_TYPE("EClassifier (Return Type"),
     THROWS_DECLARATION("EException"),
     TYPE_PARAMETER("Type Parameter"),
@@ -41,17 +41,27 @@ public enum MetamodelTokenType implements TokenType {
         return description;
     }
 
+    /**
+     * Creates a normal metamodel token type.
+     * @param description is the textual description.
+     */
     MetamodelTokenType(String description) {
         this.description = description;
         endToken = false;
     }
 
-    MetamodelTokenType(String description, boolean endToken) {
-        this.endToken = endToken;
-        this.description = endToken ? END_TOKEN_SUFFIX + description : description;
-
+    /**
+     * Create a metamodel end token type, meaning a token that indicates the end of a containment.
+     * @param beginType is the corresponding begin token.
+     */
+    MetamodelTokenType(MetamodelTokenType beginType) {
+        description = beginType.getDescription() + END_TOKEN_SUFFIX;
+        endToken = true;
     }
 
+    /**
+     * @return true if the token is a end token type, meaning a token that indicates the end of a containment.
+     */
     public boolean isEndToken() {
         return endToken;
     }
