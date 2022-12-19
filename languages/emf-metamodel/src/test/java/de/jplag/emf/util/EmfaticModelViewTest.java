a7c481b0c2f850dbfe156d04cce7f609b3363ef9
Timur Sağlam <timur.saglam@kit.edu>
2022-12-19 17:00:18 +0100

    Replace single test case with parameterized test.

--- languages/emf-metamodel/src/test/java/de/jplag/emf/util/EmfaticModelViewTest.java
+++ languages/emf-metamodel/src/test/java/de/jplag/emf/util/EmfaticModelViewTest.java
@@ -9,45 +9,48 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
 
 import org.eclipse.emf.ecore.resource.Resource;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import de.jplag.emf.AbstractEmfTest;
 import de.jplag.emf.Language;
 
 class EmfaticModelViewTest extends AbstractEmfTest {
 
-    private static final String EXPECTED_VIEWS = "emfatic";
+    private static final String EXPECTED_VIEW_FOLDER = "emfatic";
 
-    @Test
-    @DisplayName("Test content of emfatic view files of example metamodels")
-    void testEmfaticViewFiles() {
-
-        for (String modelName : TEST_SUBJECTS) {
-            // Load models:
-            File modelFile = new File(baseDirectory, modelName);
-            assertTrue(modelFile.exists());
-            Resource modelResource = EMFUtil.loadModelResource(modelFile);
-            assertNotNull(modelResource);
-
-            // Generate emfatic view:
-            EmfaticModelView view = new EmfaticModelView(modelFile, modelResource);
-            view.writeToFile(Language.VIEW_FILE_SUFFIX);
-
-            // Compare expected vs. actual view file:
-            File viewFile = new File(modelFile.getPath() + Language.VIEW_FILE_SUFFIX);
-            assertTrue(viewFile.exists());
-            File expectedViewFile = BASE_PATH.resolveSibling(Path.of(EXPECTED_VIEWS, viewFile.getName())).toFile();
-            assertTrue(expectedViewFile.exists());
-            try {
-                assertEquals(Files.readString(expectedViewFile.toPath()), Files.readString(viewFile.toPath()));
-            } catch (IOException exception) {
-                fail(exception);
-            }
-        }
-
+    private static List<String> provideModelNames() {
+        return Arrays.asList(TEST_SUBJECTS);
     }
 
+    @ParameterizedTest
+    @DisplayName("Test content of emfatic view files of example metamodels")
+    @MethodSource("provideModelNames")
+    void testEmfaticViewFiles(String modelName) {
+        // Load model:
+        File modelFile = new File(baseDirectory, modelName);
+        assertTrue(modelFile.exists());
+        Resource modelResource = EMFUtil.loadModelResource(modelFile);
+        assertNotNull(modelResource);
+
+        // Generate emfatic view:
+        EmfaticModelView view = new EmfaticModelView(modelFile, modelResource);
+        view.writeToFile(Language.VIEW_FILE_SUFFIX);
+
+        // Compare expected vs. actual view file:
+        File viewFile = new File(modelFile.getPath() + Language.VIEW_FILE_SUFFIX);
+        assertTrue(viewFile.exists());
+        File expectedViewFile = BASE_PATH.resolveSibling(Path.of(EXPECTED_VIEW_FOLDER, viewFile.getName())).toFile();
+        assertTrue(expectedViewFile.exists());
+        try {
+            assertEquals(Files.readString(expectedViewFile.toPath()), Files.readString(viewFile.toPath()));
+        } catch (IOException exception) {
+            fail(exception);
+        }
+    }
 }
