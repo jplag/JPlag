1b9a2d618f216f9d766c3cab89cdf9e2d99e084c
Timur Sağlam <timur.saglam@kit.edu>
2023-02-17 16:31:37 +0100

    Add Javadoc to the Java test base.

--- languages/java/src/test/java/de/jplag/java/AbstractJavaLanguageTest.java
+++ languages/java/src/test/java/de/jplag/java/AbstractJavaLanguageTest.java
@@ -27,6 +27,9 @@ public class AbstractJavaLanguageTest {
     private de.jplag.Language language;
     protected File baseDirectory;
 
+    /**
+     * Sets up the base directory and the language module.
+     */
     @BeforeEach
     void setUp() {
         language = new Language();
@@ -34,11 +37,17 @@ public class AbstractJavaLanguageTest {
         assertTrue(baseDirectory.exists(), "Could not find base directory!");
     }
 
+    /**
+     * Parses a java file in the {@link AbstractJavaLanguageTest.baseDirectory} and returns the list of token types.
+     * @param fileName is the name of the file to parse.
+     * @return the token types.
+     * @throws ParsingException if parsing fails.
+     */
     protected List<TokenType> parseJavaFile(String fileName) throws ParsingException {
         List<Token> parsedTokens = language.parse(Set.of(new File(baseDirectory, fileName)));
         List<TokenType> tokenTypes = parsedTokens.stream().map(Token::getType).toList();
         logger.info(LOG_MESSAGE, fileName, tokenTypes);
-        logger.debug(TokenPrinter.printTokens(parsedTokens, BASE_PATH.toAbsolutePath().toFile()));
+        logger.info(TokenPrinter.printTokens(parsedTokens, BASE_PATH.toAbsolutePath().toFile()));
         return tokenTypes;
     }
 
