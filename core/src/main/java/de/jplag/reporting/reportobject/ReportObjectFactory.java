5a804315ae69495cf7da5a0c0b10f33aabf5842e
Yifei Chen <43599217+cyfml@users.noreply.github.com>
2023-02-28 16:42:43 +0100

    Update core/src/main/java/de/jplag/reporting/reportobject/ReportObjectFactory.java
    Co-authored-by: Timur Sağlam <timur.saglam@kit.edu>

--- core/src/main/java/de/jplag/reporting/reportobject/ReportObjectFactory.java
+++ core/src/main/java/de/jplag/reporting/reportobject/ReportObjectFactory.java
@@ -169,7 +169,7 @@ public class ReportObjectFactory {
         int numberOfMaximumComparisons = result.getOptions().maximumNumberOfComparisons();
         int shownComparisons = totalComparisons > numberOfMaximumComparisons ? numberOfMaximumComparisons : totalComparisons;
         int missingComparisons = totalComparisons > numberOfMaximumComparisons ? (totalComparisons - numberOfMaximumComparisons) : 0;
-        logger.info("Total Comparisons: {}. Shown Comparisons: {}. Missing Comparisons: {}.", totalComparisons, shownComparisons, missingComparisons);
+        logger.info("Total Comparisons: {}. Comparisons in Report: {}. Omitted Comparisons: {}.", totalComparisons, shownComparisons, missingComparisons);
         OverviewReport overviewReport = new OverviewReport(REPORT_VIEWER_VERSION, folders.stream().map(File::getPath).toList(), // submissionFolderPath
                 baseCodePath, // baseCodeFolderPath
                 result.getOptions().language().getName(), // language
