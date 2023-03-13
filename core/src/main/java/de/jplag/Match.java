b49f6a12c6e5fa5b7aab5a92192d1f8276d4e8cd
Timur Sağlam <timur.saglam@kit.edu>
2023-03-13 14:31:27 +0100

    Allow JPlag to correctly show matches where the token order is not identical to the line index order.

--- core/src/main/java/de/jplag/Match.java
+++ core/src/main/java/de/jplag/Match.java
@@ -2,8 +2,8 @@ package de.jplag;
 
 /**
  * Represents two sections of two submissions that are similar.
- * @param startOfFirst is the starting token index in the first submission.
- * @param startOfSecond is the starting token index in the second submission.
+ * @param startOfFirst is the index of the first token of the match in the first submission.
+ * @param startOfSecond is the index of the first token of the match in the second submission.
  * @param length is the length of these similar sections (number of tokens).
  */
 public record Match(int startOfFirst, int startOfSecond, int length) {
@@ -27,5 +27,19 @@ public record Match(int startOfFirst, int startOfSecond, int length) {
         } else {
             return (startOfSecond - other.startOfSecond) < other.length;
         }
+    }
+
+    /**
+     * @return the token index of the last token of the match in the first submission.
+     */
+    public int endOfFirst() {
+        return startOfFirst + length - 1;
+    }
+
+    /**
+     * @return the token index of the last token of the match in the second submission.
+     */
+    public int endOfSecond() {
+        return startOfSecond + length - 1;
     }
 }
