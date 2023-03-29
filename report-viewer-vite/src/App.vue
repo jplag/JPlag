4646a65ae747fd258de8c4fb949f59c7048c5645
Sebastian Hahner <hahner@kit.edu>
2023-03-29 11:09:30 +0200

    Add entry files, configs, and fonts from old report viewer.

--- report-viewer-vite/src/App.vue
+++ report-viewer-vite/src/App.vue
@@ -1,85 +1,49 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
+  <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
   <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style>
+@font-face {
+  font-family: "JetBrains Mono NL";
+  src: url("./fonts/jetbrains-mono/JetBrainsMonoNL-Light.ttf");
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+@font-face {
+  font-family: "JetBrains Mono NL";
+  src: url("./fonts/jetbrains-mono/JetBrainsMonoNL-Bold.ttf");
+  font-weight: bold;
 }
 
-nav {
+:root {
+  --primary-color: #E2EAFC;
+  --primary-color-light: #EDF2FB;
+  --primary-color-dark: #D7E3FC;
+  --secondary-color: #B6CCFE;
+  --secondary-color-light: #C1D3FE;
+  --secondary-color-dark: #ABC4FF;
+
+  --background-color: #FFFFFF;
+  --on-primary-color: #303030;
+  --on-secondary-color: #000000;
+  --on-background-color: #303030;
+  --shadow-color: #777777;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+html, body, #app {
+  height: 100%;
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  margin: 0;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- report-viewer-vite/src/main.ts
+++ report-viewer-vite/src/main.ts
@@ -3,12 +3,17 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import VueVirtualScroller from "vue-virtual-scroller"
+import 'highlight.js/lib/common';
 
-import './assets/main.css'
+import 'highlight.js/styles/vs.css'
+import 'gitart-vue-dialog/dist/style.css'
+import 'vue-virtual-scroller/dist/vue-virtual-scroller.css';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueVirtualScroller)
 
 app.mount('#app')
