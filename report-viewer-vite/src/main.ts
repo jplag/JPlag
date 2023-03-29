56f04af83572d3a4f9e737fda6a0718a414bd43a
Sebastian Hahner <hahner@kit.edu>
2023-03-29 10:38:24 +0200

    Add pinia instead of vuex. Add other dependencies from old package.json.

--- report-viewer-vite/src/main.ts
+++ report-viewer-vite/src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import App from './App.vue'
 import router from './router'
 
@@ -6,6 +8,7 @@ import './assets/main.css'
 
 const app = createApp(App)
 
+app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
