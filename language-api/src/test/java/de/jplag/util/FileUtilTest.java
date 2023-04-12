48f27eb2017f9861b17f9ca9aded2c3643068143
Alexander Milster <alexander.milster@gmx.de>
2023-04-12 12:46:08 +0200

    Added a test case for guessing the charset from multiple files.

--- language-api/src/test/java/de/jplag/util/FileUtilTest.java
+++ language-api/src/test/java/de/jplag/util/FileUtilTest.java
@@ -3,14 +3,18 @@ package de.jplag.util;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
+import java.util.Set;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class FileUtilTest {
     private static final Path TEST_FILE_LOCATION = Path.of("src", "test", "resources", "de", "jplag", "fileReaderTests");
+    private static final Path TEST_FILE_SET_LOCATION = Path.of("src", "test", "resources", "de", "jplag", "fileSetEncoding");
 
     private static final String expectedFileContent = "Some ascii characters and some others: ä#+öü%&(/)?=?";
 
@@ -27,6 +31,13 @@ public class FileUtilTest {
     public void testCharsetDetection(File file) throws IOException {
         Assertions.assertEquals(Charset.forName(file.getName()), FileUtils.detectCharset(file),
                 "Wrong charset assumed for: " + file.getAbsolutePath());
+    }
+
+    @Test
+    public void testDetectFromFileSet() {
+        Set<File> files = Set.of(TEST_FILE_SET_LOCATION.toFile().listFiles());
+        Charset encoding = FileUtils.detectCharsetFromMultiple(files);
+        Assertions.assertEquals(StandardCharsets.ISO_8859_1, encoding);
     }
 
     public static File[] searchTestFiles() {
