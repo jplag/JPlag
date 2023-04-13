1591f0d4bf5c6057808ced8d636e0cd48a1fe65c
Alexander Vogt <alexander_vogt@outlook.de>
2023-04-13 16:37:17 +0200

    Added testing boilerplates

---
+++ report-viewer-vite/tests/unit/boilderplate.test.ts
@@ -0,0 +1,16 @@
+import { assert, describe, expect, test, it, beforeEach } from 'vitest'
+import { mount } from '@vue/test-utils'
+import { setActivePinia, createPinia } from 'pinia'
+
+test('example_unit', async () => {})
+
+describe('example_component', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+
+  it('example_1_component', () => {
+    // expect(ComponentName).toBeTruthy()
+    // const wrapper = mount(ComponentName, {
+  })
+})
--- report-viewer-vite/vitest.config.ts
+++ report-viewer-vite/vitest.config.ts
@@ -8,7 +8,7 @@ export default mergeConfig(
   defineConfig({
     test: {
       environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/*'],
+      exclude: [...configDefaults.exclude, 'tests/e2e/*'],
       root: fileURLToPath(new URL('./', import.meta.url))
     }
   })
