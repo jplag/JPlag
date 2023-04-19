a5c04d5ec78c108bb1101418feeca8b2dc9995e7
Alexander Vogt <alexander_vogt@outlook.de>
2023-04-19 18:00:16 +0200

    Moved view files to composition api

--- report-viewer/src/views/ErrorView.vue
+++ report-viewer/src/views/ErrorView.vue
@@ -6,25 +6,13 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
 import ErrorMessage from '@/components/ErrorMessage.vue'
 import ErrorRouter from '@/components/ErrorRouter.vue'
 
-export default defineComponent({
-  name: 'OverviewErrorView',
-  components: { ErrorRouter, ErrorMessage },
-  setup() {
-    const message = history.state.message
-    const to = history.state.to
-    const routerInfo = history.state.routerInfo
-    return {
-      message,
-      to,
-      routerInfo
-    }
-  }
-})
+const message = history.state.message as string
+const to = history.state.to as string
+const routerInfo = history.state.routerInfo as string
 </script>
 
 <style scoped>
