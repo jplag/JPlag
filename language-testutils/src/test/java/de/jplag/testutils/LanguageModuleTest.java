89cbfa2451c00e4e991ed53a48ac12f660c51b4b
Alexander Milster <alexander.milster@gmx.de>
2023-06-06 15:19:15 +0200

    Changed the test code, so that empty tests are ignored in total.

--- language-testutils/src/test/java/de/jplag/testutils/LanguageModuleTest.java
+++ language-testutils/src/test/java/de/jplag/testutils/LanguageModuleTest.java
@@ -1,32 +1,28 @@
 package de.jplag.testutils;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.*;
-
+import de.jplag.*;
+import de.jplag.testutils.datacollector.TestData;
+import de.jplag.testutils.datacollector.TestDataCollector;
+import de.jplag.testutils.datacollector.TestSourceIgnoredLinesCollector;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import de.jplag.*;
-import de.jplag.testutils.datacollector.TestData;
-import de.jplag.testutils.datacollector.TestDataCollector;
-import de.jplag.testutils.datacollector.TestSourceIgnoredLinesCollector;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 /**
  * Base class for language module tests. Automatically adds all common tests types for jplag languages.
  */
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public abstract class LanguageModuleTest {
-    private static final Logger logger = LoggerFactory.getLogger(LanguageModuleTest.class);
     private static final Path DEFAULT_TEST_CODE_PATH_BASE = Path.of("src", "test", "resources", "de", "jplag");
 
     private final TestDataCollector collector;
@@ -35,7 +31,8 @@ public abstract class LanguageModuleTest {
 
     /**
      * Creates a new language module test
-     * @param language The language to test
+     *
+     * @param language       The language to test
      * @param languageTokens All tokens, that can be reported by the module. The end file token can be omitted.
      */
     public LanguageModuleTest(Language language, List<TokenType> languageTokens) {
@@ -46,7 +43,8 @@ public abstract class LanguageModuleTest {
 
     /**
      * Creates a new language module test
-     * @param language The language to test
+     *
+     * @param language       The language to test
      * @param languageTokens All tokens, that can be reported by the module. The end file token can be omitted.
      */
     public LanguageModuleTest(Language language, TokenType[] languageTokens) {
@@ -55,7 +53,8 @@ public abstract class LanguageModuleTest {
 
     /**
      * Creates a new language module test
-     * @param language The language to test
+     *
+     * @param language  The language to test
      * @param tokenEnum The enum containing the token types
      */
     public <T extends Enum<T> & TokenType> LanguageModuleTest(Language language, Class<T> tokenEnum) {
@@ -64,15 +63,14 @@ public abstract class LanguageModuleTest {
 
     /**
      * Test the configured source files for source line coverage
+     *
      * @param data The source to check
      * @throws ParsingException If the parser throws some error
-     * @throws IOException If any IO Exception occurs
+     * @throws IOException      If any IO Exception occurs
      */
     @ParameterizedTest
     @MethodSource("sourceCoverageFiles")
     final void testSourceCoverage(TestData data) throws ParsingException, IOException {
-        this.ignoreNullTest(data);
-
         List<Token> tokens = data.parseTokens(this.language);
 
         TestSourceIgnoredLinesCollector ignoredLines = new TestSourceIgnoredLinesCollector(data.getSourceLines());
@@ -88,23 +86,23 @@ public abstract class LanguageModuleTest {
 
     /**
      * Returns all test sources, that need to be checked for source line coverage
+     *
      * @return The test sources
      */
     final List<TestData> sourceCoverageFiles() {
-        return addNullGuard(this.collector.getSourceCoverageData());
+        return ignoreEmptyTestType(this.collector.getSourceCoverageData());
     }
 
     /**
      * Checks the configured source files for token coverage
+     *
      * @param data The source to check
      * @throws ParsingException If the parser throws some error
-     * @throws IOException If any IO Exception occurs
+     * @throws IOException      If any IO Exception occurs
      */
     @ParameterizedTest
     @MethodSource("tokenCoverageFiles")
     final void testTokenCoverage(TestData data) throws ParsingException, IOException {
-        this.ignoreNullTest(data);
-
         List<TokenType> foundTokens = data.parseTokens(this.language).stream().map(Token::getType).toList();
         List<TokenType> languageTokens = new ArrayList<>(this.languageTokens);
 
@@ -116,23 +114,23 @@ public abstract class LanguageModuleTest {
 
     /**
      * Returns all test sources, that need to be checked for token coverage.
+     *
      * @return The test sources
      */
     final List<TestData> tokenCoverageFiles() {
-        return addNullGuard(this.collector.getTokenCoverageData());
+        return ignoreEmptyTestType(this.collector.getTokenCoverageData());
     }
 
     /**
      * Tests the configured test sources for contained tokens
+     *
      * @param test The source to test
      * @throws ParsingException If the parser throws some error
-     * @throws IOException If any IO Exception occurs
+     * @throws IOException      If any IO Exception occurs
      */
     @ParameterizedTest
     @MethodSource("testTokensContainedData")
     final void testTokensContained(TestDataCollector.TokenListTest test) throws ParsingException, IOException {
-        this.ignoreNullTest(test);
-
         List<TokenType> foundTokens = test.data().parseTokens(this.language).stream().map(Token::getType).toList();
         List<TokenType> requiredTokens = new ArrayList<>(test.tokens());
 
@@ -146,23 +144,23 @@ public abstract class LanguageModuleTest {
 
     /**
      * Returns all test sources, that need to be checked for contained tokens
+     *
      * @return The test sources
      */
     final List<TestDataCollector.TokenListTest> testTokensContainedData() {
-        return addNullGuard(this.collector.getContainedTokenData());
+        return ignoreEmptyTestType(this.collector.getContainedTokenData());
     }
 
     /**
      * Checks the given test sources for an exact token sequence
+     *
      * @param test The source to check
      * @throws ParsingException If the parser throws some error
-     * @throws IOException If any IO Exception occurs
+     * @throws IOException      If any IO Exception occurs
      */
     @ParameterizedTest
     @MethodSource("testTokenSequenceData")
     final void testTokenSequence(TestDataCollector.TokenListTest test) throws ParsingException, IOException {
-        this.ignoreNullTest(test);
-
         List<TokenType> extracted = test.data().parseTokens(this.language).stream().map(Token::getType).toList();
         List<TokenType> required = new ArrayList<>(test.tokens());
         if (required.get(required.size() - 1) != SharedTokenType.FILE_END) {
@@ -175,23 +173,23 @@ public abstract class LanguageModuleTest {
 
     /**
      * Returns all test sources, that need to be checked for a matching token sequence
+     *
      * @return The test sources
      */
     final List<TestDataCollector.TokenListTest> testTokenSequenceData() {
-        return addNullGuard(this.collector.getTokenSequenceTest());
+        return ignoreEmptyTestType(this.collector.getTokenSequenceTest());
     }
 
     /**
      * Tests all configured test sources for a monotone order of tokens
+     *
      * @param data The test source
      * @throws ParsingException If the parser throws some error
-     * @throws IOException If any IO Exception occurs
+     * @throws IOException      If any IO Exception occurs
      */
     @ParameterizedTest
     @MethodSource("getAllTestData")
     final void testMonotoneTokenOrder(TestData data) throws ParsingException, IOException {
-        this.ignoreNullTest(data);
-
         List<Token> extracted = data.parseTokens(this.language);
 
         for (int i = 0; i < extracted.size() - 2; i++) {
@@ -207,15 +205,14 @@ public abstract class LanguageModuleTest {
 
     /**
      * Checks that all configured test sources end with a FileEnd token
+     *
      * @param data The test source
      * @throws ParsingException If the parser throws some error
-     * @throws IOException If any IO Exception occurs
+     * @throws IOException      If any IO Exception occurs
      */
     @ParameterizedTest
     @MethodSource("getAllTestData")
     final void testTokenSequencesEndsWithFileEnd(TestData data) throws ParsingException, IOException {
-        this.ignoreNullTest(data);
-
         List<Token> extracted = data.parseTokens(this.language);
 
         Assertions.assertEquals(SharedTokenType.FILE_END, extracted.get(extracted.size() - 1).getType(),
@@ -224,10 +221,11 @@ public abstract class LanguageModuleTest {
 
     /**
      * Returns all configured test sources
+     *
      * @return The test sources
      */
     final Set<TestData> getAllTestData() {
-        return addNullGuard(this.collector.getAllTestData());
+        return ignoreEmptyTestType(this.collector.getAllTestData());
     }
 
     /**
@@ -239,41 +237,35 @@ public abstract class LanguageModuleTest {
     }
 
     /**
-     * Adds null to the data, if it is empty. This is to prevent JUnit from failing when no tests parameters are configured
-     * for a specific test.
-     * @param data The list to modify
-     * @param <T> The type of items
-     * @param <C> The collection type
-     * @return The modified list
+     * Ignores the test, if there is no data, by failing an assumption.
+     *
+     * @param data The list containing the test data
+     * @param <T>  The type of items
+     * @param <C>  The collection type
+     * @return The data
      */
-    private <T, C extends Collection<T>> C addNullGuard(C data) {
-        if (data.size() == 0) {
-            data.add(null);
-
-            logger.info("No tests configured. Creating a default null test, to fulfill JUnits requirements.");
-        }
-
+    private <T, C extends Collection<T>> C ignoreEmptyTestType(C data) {
+        Assumptions.assumeFalse(data.isEmpty(), "Ignoring empty test type.");
         return data;
-    }
-
-    private void ignoreNullTest(Object parameter) {
-        Assumptions.assumeTrue(parameter != null);
     }
 
     /**
      * Collects all tests, that should be executed.
+     *
      * @param collector Use to collect the tests
      */
     abstract protected void collectTestData(TestDataCollector collector);
 
     /**
      * Configure which lines should not be checked for source coverage.
+     *
      * @param collector Used to ignore lines
      */
     abstract protected void configureIgnoredLines(TestSourceIgnoredLinesCollector collector);
 
     /**
      * Returns the default directory structure by default.
+     *
      * @return The test file location
      */
     protected File getTestFileLocation() {
