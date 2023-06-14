bcbeb709861d5228563649dfef54620493aeb981
Alexander Vogt <alexander_vogt@outlook.de>
2023-06-14 16:53:06 +0200

    removed unused packages

--- report-viewer/src/main.ts
+++ report-viewer/src/main.ts
@@ -6,7 +6,6 @@ import router from './router'
 import VueVirtualScroller from 'vue-virtual-scroller'
 import 'highlight.js/lib/common'
 
-import 'gitart-vue-dialog/dist/style.css'
 import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
 
 import './style.css'
