a9b37c4f8ced4733c54c4926e57e6ab290ed8471
uuqjz <uuqjz@student.kit.edu>
2023-06-30 06:54:45 +0200

    Removed framework for Token Rules

--- core/src/main/java/de/jplag/merging/Altering.java
+++ core/src/main/java/de/jplag/merging/Altering.java
@@ -9,7 +9,6 @@ import de.jplag.SubmissionSet;
 import de.jplag.options.JPlagOptions;
 import de.jplag.Token;
 import de.jplag.TokenType;
-//import de.jplag.java.JavaTokenType;
 
 
 public class Altering{
@@ -19,17 +18,14 @@ public class Altering{
     private Submission submission;
     private List<Token> tokenList;
     private Random rand;
-    private String language;
     private List<TokenType> typeDict;
-    private Boolean rules;
+
     
     public Altering(SubmissionSet s,JPlagOptions o) {
         submissionSet=s;
         submissions=submissionSet.getSubmissions();
         options=o;
         rand = new Random(1337);
-        language=options.language().getIdentifier();
-        rules=false;
     }
     
     public void run() {
@@ -39,17 +35,7 @@ public class Altering{
             submission=submissions.get(i);
             if(submission.getName().startsWith("a_")) {
                 tokenList=new ArrayList<>(submission.getTokenList());
-                if(!rules) {
-                    randomAlteration(); 
-                }
-                else {
-                    if(language.equals("java")) {
-                        applyJavaRules();
-                    }
-                    if(language.equals("cpp")) {
-                        applyCPPRules();
-                    }
-                } 
+                randomAlteration(); 
                 submission.setTokenList(tokenList); 
             }  
         }
@@ -58,27 +44,11 @@ public class Altering{
     private void randomAlteration() {
         //Ignore FILE_END
         for(int i=0;i < tokenList.size()-1;i++) {
-            if (rand.nextInt(10) == 0) {
+            //20% Chance of Alteration
+            if (rand.nextInt(10) <= 1) {
                 tokenList.get(i).setType(typeDict.get(rand.nextInt(typeDict.size())));
             }
         }
-    }
-    
-    private void applyJavaRules() {
-        for(int i=0;i < tokenList.size();i++) {
-            if(ControlTokenTypes.J_DO_BEGIN.equals(tokenList.get(i).getType())) {
-                System.out.println("Token is J_DO_BEGIN");
-                //tokenList.get(i).setType(ControlTokenTypes.J_APPLY);
-            } 
-        }  
-    }
-    
-    private void applyCPPRules() {
-        for(int i=0;i < tokenList.size();i++) {
-            if(ControlTokenTypes.C_DO.equals(tokenList.get(i).getType())) {
-                System.out.println("Token is C_DO");
-            } 
-        } 
     }
     
     private void fillTypeDict() {
