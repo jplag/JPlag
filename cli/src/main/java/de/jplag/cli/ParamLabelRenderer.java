c9a44073700fcf3eb15642165142b5264196139a
Alexander Milster <alexander.milster@gmx.de>
2023-08-04 11:58:47 +0200

    Spotless.

--- cli/src/main/java/de/jplag/cli/ParamLabelRenderer.java
+++ cli/src/main/java/de/jplag/cli/ParamLabelRenderer.java
@@ -1,10 +1,10 @@
 package de.jplag.cli;
 
-import picocli.CommandLine;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import picocli.CommandLine;
 
 public class ParamLabelRenderer implements CommandLine.Help.IParamLabelRenderer {
     private final CommandLine.Help.IParamLabelRenderer base;
@@ -14,9 +14,11 @@ public class ParamLabelRenderer implements CommandLine.Help.IParamLabelRenderer 
     }
 
     @Override
-    public CommandLine.Help.Ansi.Text renderParameterLabel(CommandLine.Model.ArgSpec argSpec, CommandLine.Help.Ansi ansi, List<CommandLine.Help.Ansi.IStyle> styles) {
-        if(argSpec.type().isEnum()) {
-            @SuppressWarnings("unchecked") Enum<?>[] enumConstants = ((Class<Enum<?>>)argSpec.type()).getEnumConstants();
+    public CommandLine.Help.Ansi.Text renderParameterLabel(CommandLine.Model.ArgSpec argSpec, CommandLine.Help.Ansi ansi,
+            List<CommandLine.Help.Ansi.IStyle> styles) {
+        if (argSpec.type().isEnum()) {
+            @SuppressWarnings("unchecked")
+            Enum<?>[] enumConstants = ((Class<Enum<?>>) argSpec.type()).getEnumConstants();
             String enumValueNames = Arrays.stream(enumConstants).map(Enum::name).collect(Collectors.joining(","));
             return CommandLine.Help.Ansi.AUTO.text(String.format("=<{%s}>", enumValueNames));
         }
