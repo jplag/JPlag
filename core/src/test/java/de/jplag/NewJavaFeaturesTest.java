7cbebbe4f5cf1a786aca0114ef30b45e9933de2a
Timur Saglam <timur.saglam@kit.edu>
2023-08-08 11:42:56 +0200

    Improve error messages of the test for new java features.

--- core/src/test/java/de/jplag/NewJavaFeaturesTest.java
+++ core/src/test/java/de/jplag/NewJavaFeaturesTest.java
@@ -4,20 +4,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import de.jplag.exceptions.ExitException;
 
 public class NewJavaFeaturesTest extends TestBase {
+
+    private final Logger logger = LoggerFactory.getLogger(NewJavaFeaturesTest.class);
+
     private static final int EXPECTED_MATCHES = 6; // might change if you add files to the submissions
     private static final double EXPECTED_SIMILARITY = 0.96; // might change if you add files to the submissions
+    private static final String EXPECTED_JAVA_VERSION = "17"; // might change with newer JPlag versions
 
     private static final String EXCLUSION_FILE_NAME = "blacklist.txt";
     private static final String ROOT_DIRECTORY = "NewJavaFeatures";
     private static final String CHANGE_MESSAGE = "Number of %s changed! If intended, modify the test case!";
+    private static final String VERSION_MISMATCH_MESSAGE = "Using Java version %s instead of %s may skew the results";
+    private static final String VERSION_MATCH_MESSAGE = "Java version matches, but results deviate from expected values";
+    private static final String JAVA_VERSION_KEY = "java.version";
 
     @Test
     @DisplayName("test comparison of Java files with modern language features")
     public void testJavaFeatureDuplicates() throws ExitException {
+
         JPlagResult result = runJPlagWithExclusionFile(ROOT_DIRECTORY, EXCLUSION_FILE_NAME);
 
         // Ensure test input did not change:
@@ -29,8 +39,19 @@ public class NewJavaFeaturesTest extends TestBase {
 
         // Check similarity and number of matches:
         var comparison = result.getAllComparisons().get(0);
-        assertEquals(EXPECTED_SIMILARITY, comparison.similarity(), DELTA);
-        assertEquals(EXPECTED_MATCHES, comparison.matches().size());
+        String versionMessage = createJavaVersionMessage();
+        assertEquals(EXPECTED_SIMILARITY, comparison.similarity(), DELTA, versionMessage);
+        assertEquals(EXPECTED_MATCHES, comparison.matches().size(), versionMessage);
+    }
+
+    private String createJavaVersionMessage() {
+        String actualJavaVersion = System.getProperty(JAVA_VERSION_KEY);
+        String message = VERSION_MATCH_MESSAGE;
+        if (!actualJavaVersion.startsWith(EXPECTED_JAVA_VERSION)) {
+            message = VERSION_MISMATCH_MESSAGE.formatted(actualJavaVersion, EXPECTED_JAVA_VERSION);
+            logger.error(message);
+        }
+        return message;
     }
 
 }
