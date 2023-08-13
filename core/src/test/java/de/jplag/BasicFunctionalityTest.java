8325790b768da39ccb7fece3f8ffb7726013fe19
Alex | Kronox <alexander_vogt@outlook.de>
2023-08-13 19:15:34 +0200

    spottless

--- core/src/test/java/de/jplag/BasicFunctionalityTest.java
+++ core/src/test/java/de/jplag/BasicFunctionalityTest.java
@@ -15,6 +15,7 @@ import de.jplag.exceptions.ExitException;
 class BasicFunctionalityTest extends TestBase {
 
     private static int DISTRIBUTION_INDEX = 66;
+
     @Test
     @DisplayName("test submissions that contain obvious plagiarism")
     void testSimpleDuplicate() throws ExitException {
