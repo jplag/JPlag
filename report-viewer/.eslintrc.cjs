4a93d542f6e76e0a37d7f85a3254dc026776ef6a
Alex | Kronox <alexander_vogt@outlook.de>
2023-08-24 17:54:43 +0200

    add rule

--- report-viewer/.eslintrc.cjs
+++ report-viewer/.eslintrc.cjs
@@ -13,6 +13,7 @@ module.exports = {
     ecmaVersion: 'latest'
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/no-setup-props-reactivity-loss": "error"
   }
 }
