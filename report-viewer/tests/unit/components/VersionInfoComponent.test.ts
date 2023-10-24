47f1ad4e4ee03b257e49aeb059df11602180091a
Alexander Vogt <alexander_vogt@outlook.de>
2023-10-24 08:37:42 +0200

    update tests

--- report-viewer/tests/unit/components/VersionInfoComponent.test.ts
+++ report-viewer/tests/unit/components/VersionInfoComponent.test.ts
@@ -8,32 +8,44 @@ vi.mock('@/version.json')
 describe('VersionInfoComponent', () => {
   it('Render develop version', async () => {
     version.report_viewer_version = mockVersionJSON(0, 0, 0)
+    version.minimal_report_version = mockVersionJSON(4, 0, 0)
     global.fetch = vi.fn().mockResolvedValueOnce(mockVersionResponse('v4.3.0'))
 
     const wrapper = mount(VersionInfoComponent)
     await flushPromises()
 
     expect(wrapper.text()).toContain('development version')
+    expect(wrapper.text()).not.toContain(
+      'The minimal version of JPlag that is supported by the viewer is v4.0.0.'
+    )
   })
 
   it('Render outdated version', async () => {
     version.report_viewer_version = mockVersionJSON(4, 3, 0)
+    version.minimal_report_version = mockVersionJSON(4, 0, 0)
     global.fetch = vi.fn().mockResolvedValueOnce(mockVersionResponse('v4.4.0'))
 
     const wrapper = mount(VersionInfoComponent)
     await flushPromises()
 
     expect(wrapper.text()).toContain('outdated version')
+    expect(wrapper.text()).toContain(
+      'The minimal version of JPlag that is supported by the viewer is v4.0.0.'
+    )
   })
 
   it('Render latest version', async () => {
     version.report_viewer_version = mockVersionJSON(4, 3, 0)
+    version.minimal_report_version = mockVersionJSON(4, 0, 0)
     global.fetch = vi.fn().mockResolvedValueOnce(mockVersionResponse('v4.3.0'))
 
     const wrapper = mount(VersionInfoComponent)
     await flushPromises()
 
     expect(wrapper.text()).toContain('JPlag v4.3.0')
+    expect(wrapper.text()).toContain(
+      'The minimal version of JPlag that is supported by the viewer is v4.0.0.'
+    )
   })
 })
 
