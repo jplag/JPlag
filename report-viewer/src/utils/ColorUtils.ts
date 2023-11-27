22827f0a5dc6a887c3e95afe2526dc822d983025
Alex | Kronox <alexander_vogt@outlook.de>
2023-11-27 14:08:19 +0100

    switch color

--- report-viewer/src/utils/ColorUtils.ts
+++ report-viewer/src/utils/ColorUtils.ts
@@ -63,7 +63,7 @@ function generateColorsForInterval(
 const matchColors: { red: number; green: number; blue: number }[] = [
   { red: 255, green: 61, blue: 0 },
   { red: 0, green: 133, blue: 255 },
-  { red: 255, green: 0, blue: 122 },
+  { red: 255, green: 122, blue: 0 },
   { red: 255, green: 245, blue: 0 },
   { red: 0, green: 255, blue: 255 },
   { red: 112, green: 0, blue: 255 },
