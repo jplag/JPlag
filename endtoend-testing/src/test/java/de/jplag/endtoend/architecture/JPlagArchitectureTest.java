12e8192ce7d2a6014d66c57cafa628325ac34653
Dominik Fuchß <develop@fuchss.org>
2023-12-03 17:02:44 +0100

    Enforce Logger Attribute Name

--- endtoend-testing/src/test/java/de/jplag/endtoend/architecture/JPlagArchitectureTest.java
+++ endtoend-testing/src/test/java/de/jplag/endtoend/architecture/JPlagArchitectureTest.java
@@ -1,5 +1,6 @@
 package de.jplag.endtoend.architecture;
 
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.fields;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 
 import com.tngtech.archunit.junit.AnalyzeClasses;
@@ -11,4 +12,7 @@ public class JPlagArchitectureTest {
     @ArchTest
     public static final ArchRule enforceCorrectLogger = noClasses().should().accessClassesThat()
             .haveNameMatching(java.util.logging.Logger.class.getName());
+
+    @ArchTest
+    public static final ArchRule enforceNameOfLogger = fields().that().haveRawType(org.slf4j.Logger.class).should().haveName("logger");
 }
