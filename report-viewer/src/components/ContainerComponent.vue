a15c417e2b38ef2fe53910eab286deaf0e12b88c
Alex | Kronox <alexander_vogt@outlook.de>
2024-01-24 11:34:52 +0100

    fix prettier reorder

--- report-viewer/src/components/ContainerComponent.vue
+++ report-viewer/src/components/ContainerComponent.vue
@@ -3,7 +3,7 @@
 -->
 <template>
   <div
-    class="box-border rounded-md border-1 border-container-border-light bg-container-light p-2 shadow print:border-2 print:shadow-none dark:border-container-border-dark dark:bg-container-dark dark:shadow-black"
+    class="box-border rounded-md border-1 border-container-border-light bg-container-light p-2 shadow dark:border-container-border-dark dark:bg-container-dark dark:shadow-black print:border-2 print:shadow-none"
   >
     <slot></slot>
   </div>
