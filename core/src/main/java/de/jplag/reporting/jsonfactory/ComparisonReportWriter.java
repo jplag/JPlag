ce32991bd8a22d94058c468b8f80d84b7415060c
Alex | Kronox <alexander_vogt@outlook.de>
2024-02-13 18:01:02 +0100

    remove unused import

--- core/src/main/java/de/jplag/reporting/jsonfactory/ComparisonReportWriter.java
+++ core/src/main/java/de/jplag/reporting/jsonfactory/ComparisonReportWriter.java
@@ -3,7 +3,6 @@ package de.jplag.reporting.jsonfactory;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
