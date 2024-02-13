459c5989c0f25741b12ad7a8ee43191313360b89
Timur Saglam <timur.saglam@kit.edu>
2024-02-13 10:42:22 +0100

    Overhaul generic EMF model view to derive more information from an EMF-based structure thus generating a simple textual view.

--- languages/emf-metamodel/src/main/java/de/jplag/emf/util/GenericEmfTreeView.java
+++ languages/emf-metamodel/src/main/java/de/jplag/emf/util/GenericEmfTreeView.java
@@ -2,10 +2,12 @@ package de.jplag.emf.util;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.StringJoiner;
 
 import org.eclipse.emf.ecore.ENamedElement;
 import org.eclipse.emf.ecore.EObject;
@@ -51,6 +53,13 @@ public class GenericEmfTreeView extends AbstractModelView {
     }
 
     private final class TreeViewBuilder extends AbstractMetamodelVisitor {
+        private static final String VALUE_ASSIGNMENT = "=";
+        private static final String COLLECTION_PREFIX = "[";
+        private static final String COLLECTION_SUFFIX = "]";
+        private static final String COLLECTION_DELIMITER = ", ";
+        private static final int ABBREVIATION_LIMIT = 15;
+        private static final String ABBREVIATION_SUFFIX = "...";
+        private static final String TEXT_AFFIX = "\"";
         private static final String IDENTIFIER_FEATURE = "name";
         private static final String INDENTATION = "  ";
         private static final String NAME_SEPARATOR = " : ";
@@ -58,23 +67,88 @@ public class GenericEmfTreeView extends AbstractModelView {
         @Override
         protected void visitEObject(EObject eObject) {
             String prefix = INDENTATION.repeat(getCurrentTreeDepth());
-            String line = prefix;
+            StringBuilder line = new StringBuilder(prefix);
+
+            // build element identifier(s):
+            List<String> identifiers = deriveNameOrIdentifers(eObject);
+            StringJoiner joiner = new StringJoiner(NAME_SEPARATOR);
+            identifiers.forEach(joiner::add);
+            line.append(joiner.toString());
+            line.append(eObject.eClass().getName()); // Build element type:
+            visitStructuralFeatures(eObject, line);  // Build element features:
+
+            lines.add(line.toString());
+            viewBuilder.append(line + System.lineSeparator());
+            // line and column values are one-indexed
+            TokenTrace trace = new TokenTrace(lines.size(), prefix.length() + 1, line.toString().trim().length());
+            objectToLine.put(eObject, trace);
+        }
+
+        private void visitStructuralFeatures(EObject eObject, StringBuilder line) {
+            List<EStructuralFeature> structuralFeatures = eObject.eClass().getEAllStructuralFeatures();
+            if (!structuralFeatures.isEmpty()) {
+                line.append(": ");
+                StringJoiner joiner = new StringJoiner(COLLECTION_DELIMITER);
+                for (EStructuralFeature feature : structuralFeatures) {
+                    Object value = eObject.eGet(feature);
+                    String name = featureValueToString(value);
+                    if (name != null) {
+                        joiner.add(feature.getName() + VALUE_ASSIGNMENT + name);
+                    }
+                }
+                line.append(joiner.toString());
+
+            }
+        }
+
+        private String featureValueToString(Object value) {
+            String name = null;
+            if (value != null) {
+                if (value instanceof EObject featureValue) {
+                    List<String> valueIdentifiers = deriveNameOrIdentifers(featureValue);
+                    if (!valueIdentifiers.isEmpty()) {
+                        name = valueIdentifiers.get(0);
+                    } else {
+                        name = featureValue.eClass().getName();
+                    }
+                } else if (value instanceof Collection<?> multipleValues) {
+                    name = valueListToString(multipleValues);
+                } else {
+
+                    name = value.toString();
+                    name = (name.length() > ABBREVIATION_LIMIT) ? name.substring(0, ABBREVIATION_LIMIT) + ABBREVIATION_SUFFIX : name;
+                    name = TEXT_AFFIX + name + TEXT_AFFIX;
+
+                }
+            }
+            return name;
+        }
+
+        private String valueListToString(Collection<?> multipleValues) {
+            String name = null;
+            if (!multipleValues.isEmpty()) {
+                name = COLLECTION_PREFIX;
+                StringJoiner joiner = new StringJoiner(COLLECTION_DELIMITER);
+                for (Object innerValue : multipleValues) {
+                    joiner.add(featureValueToString(innerValue));
+                }
+                name += joiner.toString() + COLLECTION_SUFFIX;
+            }
+            return name;
+        }
+
+        private static List<String> deriveNameOrIdentifers(EObject eObject) {
+            List<String> names = new ArrayList<>();
             if (eObject instanceof ENamedElement element) {
-                line += element.getName() + NAME_SEPARATOR;
+                names.add(element.getName());
             } else {
                 for (EStructuralFeature feature : eObject.eClass().getEAllStructuralFeatures()) {
                     if (feature.getName().toLowerCase().matches(IDENTIFIER_FEATURE) && eObject.eGet(feature) != null) {
-                        line += eObject.eGet(feature).toString() + NAME_SEPARATOR;
+                        names.add(eObject.eGet(feature).toString());
                     }
                 }
             }
-            line += eObject.eClass().getName();
-
-            lines.add(line);
-            viewBuilder.append(line + System.lineSeparator());
-            // line and column values are one-indexed
-            TokenTrace trace = new TokenTrace(lines.size(), prefix.length() + 1, line.trim().length());
-            objectToLine.put(eObject, trace);
+            return names;
         }
     }
 
