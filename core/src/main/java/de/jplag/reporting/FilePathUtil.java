97aec31f39ab6d8a6ff31e2a55903ca8e5403c81
Alexander Milster <alexander.milster@gmx.de>
2024-02-16 13:58:50 +0100

    Fixed a bug in the creation of the result due to missing slashes.

--- core/src/main/java/de/jplag/reporting/FilePathUtil.java
+++ core/src/main/java/de/jplag/reporting/FilePathUtil.java
@@ -26,4 +26,23 @@ public final class FilePathUtil {
         return Path.of(submissionToIdFunction.apply(submission), submission.getRoot().toPath().relativize(file.toPath()).toString()).toString();
     }
 
+    /**
+     * Joins logical paths using a slash. This method ensures, that no duplicate slashes are created in between.
+     * @param left The left path segment
+     * @param right The right path segment
+     * @return The joined paths
+     */
+    public static String joinPathSegments(String left, String right) {
+        String rightStripped = right;
+        while (rightStripped.startsWith("/")) {
+            rightStripped = rightStripped.substring(1);
+        }
+
+        String leftStripped = left;
+        while (leftStripped.endsWith("/")) {
+            leftStripped = leftStripped.substring(0, leftStripped.length() - 1);
+        }
+
+        return leftStripped + "/" + rightStripped;
+    }
 }
