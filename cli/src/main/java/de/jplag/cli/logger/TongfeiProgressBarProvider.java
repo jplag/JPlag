c9b300796ebae0d25f77448ac60121075dae2a5e
Alexander Milster <alexander.milster@gmx.de>
2024-02-20 16:10:20 +0100

    Changed progress bar style to ascii

--- cli/src/main/java/de/jplag/cli/logger/TongfeiProgressBarProvider.java
+++ cli/src/main/java/de/jplag/cli/logger/TongfeiProgressBarProvider.java
@@ -14,7 +14,7 @@ public class TongfeiProgressBarProvider implements ProgressBarProvider {
     @Override
     public ProgressBar initProgressBar(ProgressBarType type, int totalSteps) {
         me.tongfei.progressbar.ProgressBar progressBar = new ProgressBarBuilder().setTaskName(type.getDefaultText()).setInitialMax(totalSteps)
-                .setStyle(ProgressBarStyle.UNICODE_BLOCK).build();
+                .setStyle(ProgressBarStyle.ASCII).build();
         return new TongfeiProgressBar(progressBar);
     }
 }
