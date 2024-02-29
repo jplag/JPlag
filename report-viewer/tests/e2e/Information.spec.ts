985a68d3b11f814b3be9d543dccad10437113501
Alex | Kronox <alexander_vogt@outlook.de>
2024-02-29 18:23:21 +0100

    adapt e2e tests to progpedia dataset

--- report-viewer/tests/e2e/Information.spec.ts
+++ report-viewer/tests/e2e/Information.spec.ts
@@ -3,32 +3,32 @@ import { uploadFile } from './TestUtils'
 
 test('Test information page', async ({ page }) => {
   await page.goto('/')
-  await uploadFile('result_small_cluster.zip', page)
+  await uploadFile('progpedia.zip', page)
 
   // check displayed information on overview page
   const bodyOverview = await page.locator('body').textContent()
-  expect(bodyOverview).toContain('Directory: files')
-  expect(bodyOverview).toContain('Total Submissions: 4')
-  expect(bodyOverview).toContain('Total Comparisons: 6')
-  expect(bodyOverview).toContain('Min Token Match: 9')
+  expect(bodyOverview).toContain('Directory: ')
+  expect(bodyOverview).toMatch(/Total Submissions: [0-9]+/)
+  expect(bodyOverview).toMatch(/Total Comparisons: [0-9]+/)
+  expect(bodyOverview).toMatch(/Min Token Match: [0-9]+/)
 
   // go to information page
   await page.getByText('More', { exact: true }).click()
   await page.waitForURL('/info')
 
   // check displayed run options on information page
-  const runOptions = await page.getByText('Run Options:Submission Directory:').textContent()
-  expect(runOptions).toContain('Submission Directory: files')
-  expect(runOptions).toContain('Basecode Directory:')
-  expect(runOptions).toContain('Language: Javac based AST plugin')
-  expect(runOptions).toContain('File Extensions: .java, .JAVA')
-  expect(runOptions).toContain('Min Token Match: 9')
+  const runOptions = await page.getByText('Run Options:Language:').textContent()
+  expect(runOptions).toContain('Submission Directories: ')
+  expect(runOptions).toContain('Base Directory: ')
+  expect(runOptions).toContain('Language: ')
+  expect(runOptions).toContain('File Suffixes: ')
+  expect(runOptions).toMatch(/Min Token Match: [0-9]+/)
 
   const runData = await page.getByText('Run Data:Date of Execution:').textContent()
-  expect(runData).toContain('Date of Execution: 02/09/23')
-  expect(runData).toContain('Execution Duration: 12 ms')
-  expect(runData).toContain('Total Submissions: 4')
-  expect(runData).toContain('Total Comparisons: 6')
-  expect(runData).toContain('Shown Comparisons: 6')
-  expect(runData).toContain('Missing Comparisons: 0')
+  expect(runData).toMatch(/Date of Execution: [0-9]{2}\/[0-9]{2}\/[0-9]{2}/)
+  expect(runData).toMatch(/Execution Duration: [0-9]+ ms/)
+  expect(runData).toMatch(/Total Submissions: [0-9]+/)
+  expect(runData).toMatch(/Total Comparisons: [0-9]+/)
+  expect(runData).toMatch(/Shown Comparisons: [0-9]+/)
+  expect(runData).toMatch(/Missing Comparisons: [0-9]+/)
 })
