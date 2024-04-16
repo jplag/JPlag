c1b5304df5e85c8c53259143510eeef22ef85b72
asturnley <astyellow@aol.com>
2024-04-16 10:16:48 -0400

    Change type to PropType on line 110

--- report-viewer/src/views/OverviewView.vue
+++ report-viewer/src/views/OverviewView.vue
@@ -107,7 +107,7 @@
             />
             <TextInformation label="linear" class="flex-grow-0">
               <template #default>
-                {{ DistributionDiagram.xScale.type == 'linear' }}
+                {{ DistributionDiagram.xScale.PropType == 'linear' }}
               </template>
               <template #tooltip>
                 <div class="whitespace-pre text-sm">
