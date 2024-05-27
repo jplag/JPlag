87599c713eec4f79d70f93e9e47b5b123021f732
Timur Saglam <timur.saglam@kit.edu>
2024-05-27 10:13:51 +0200

    Clean up language utils, antlr utils, and Java E2E tests.

--- language-testutils/src/test/java/de/jplag/testutils/datacollector/FileTestData.java
+++ language-testutils/src/test/java/de/jplag/testutils/datacollector/FileTestData.java
@@ -17,7 +17,7 @@ import de.jplag.util.FileUtils;
 class FileTestData implements TestData {
     private final File file;
 
-    public FileTestData(File file) {
+    FileTestData(File file) {
         this.file = file;
     }
 
