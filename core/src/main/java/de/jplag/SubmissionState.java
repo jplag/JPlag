d51dd0488c37a3f4d96acea897ab8c0df7502d20
Timur Saglam <timur.saglam@kit.edu>
2024-05-28 12:02:20 +0200

    Move minimal length check into submission, enforce non-null token list, remove hasErrors() as it is replaced by getState().

--- core/src/main/java/de/jplag/SubmissionState.java
+++ core/src/main/java/de/jplag/SubmissionState.java
@@ -1,5 +1,9 @@
 package de.jplag;
 
+/**
+ * Reflects the state of a submission. At the beginning it is UNPARSED. After successful parsing it is VALID. In all
+ * other cases it reflects why it is inavalid.
+ */
 public enum SubmissionState {
     VALID,
     NOTHING_TO_PARSE,
