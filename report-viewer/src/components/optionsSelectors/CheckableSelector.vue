95dfdc17fd54c88c9d8a85e237ae2298ef1c1655
Alexander Vogt <alexander_vogt@outlook.de>
2024-05-28 14:27:58 +0200

    add checkable option

---
+++ report-viewer/src/components/optionsSelectors/CheckableSelector.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="flex h-fit flex-row items-center text-center text-xs">
+    <div v-if="title != ''" class="mr-3 text-base">
+      {{ title }}
+    </div>
+    <div>
+      <ToolTipComponent
+        v-if="(option as ToolTipLabel).displayValue !== undefined"
+        direction="right"
+        :tool-tip-container-will-be-centered="true"
+      >
+        <template #default>
+          <OptionComponent
+            :label="(option as ToolTipLabel).displayValue"
+            :selected="value"
+            @click="value = !value"
+          />
+        </template>
+
+        <template #tooltip>
+          <p class="whitespace-pre text-sm">
+            {{ (option as ToolTipLabel).tooltip }}
+          </p>
+        </template>
+      </ToolTipComponent>
+      <OptionComponent v-else :label="option as string" :selected="value" @click="value = !value" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { ToolTipLabel } from '@/model/ui/ToolTip'
+import { computed, ref, type PropType, type Ref, watch } from 'vue'
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: false,
+    default: ''
+  },
+  option: {
+    type: Object as PropType<string | ToolTipLabel>,
+    required: true
+  },
+  modelValue: {
+    type: Boolean,
+    required: false,
+    default: false
+  }
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const _value: Ref<boolean | undefined> = ref(undefined)
+const value = computed({
+  get: () => _value.value ?? props.modelValue,
+  set: (val: boolean) => {
+    _value.value = val
+    emit('update:modelValue', val)
+  }
+})
+
+watch(
+  () => props.modelValue,
+  (val) => {
+    _value.value = val
+  }
+)
+</script>
