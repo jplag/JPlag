848b05580599686a123e6a2ba2af91d5b5cda555
Timur Saglam <timur.saglam@kit.edu>
2024-06-25 09:54:14 +0200

    Reorder members after merging logger classes.

--- cli/src/main/java/de/jplag/cli/logger/CollectedLogger.java
+++ cli/src/main/java/de/jplag/cli/logger/CollectedLogger.java
@@ -17,33 +17,19 @@ import org.slf4j.helpers.MessageFormatter;
 public class CollectedLogger extends AbstractLogger {
 
     private static final String JPLAG_LOGGER_PREFIX = "de.jplag.";
-
     private static final Level LOG_LEVEL_FOR_EXTERNAL_LIBRARIES = Level.ERROR;
-
     private static final int MAXIMUM_MESSAGE_LENGTH = 32;
-
     private static final PrintStream TARGET_STREAM = System.out;
+
+    private final transient SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-hh:mm:ss_SSS");
+    private final ConcurrentLinkedDeque<LogEntry> allErrors = new ConcurrentLinkedDeque<>();
+    private final Level currentLogLevel;
 
     /**
      * Indicator whether finalization is in progress.
      * @see #printAllErrorsForLogger()
      */
     private transient boolean isFinalizing = false;
-
-    private final transient SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-hh:mm:ss_SSS");
-
-    private final ConcurrentLinkedDeque<LogEntry> allErrors = new ConcurrentLinkedDeque<>();
-
-    private final Level currentLogLevel;
-
-    /**
-     * Creates a logger with a specific name and {@link Level.INFO}.
-     * @param currentLogLevel is the current log level.
-     * @param name is the name of the logger.
-     */
-    CollectedLogger(String name) {
-        this(Level.INFO, name);
-    }
 
     /**
      * Creates a logger with a specific name and level.
@@ -55,60 +41,13 @@ public class CollectedLogger extends AbstractLogger {
         this.name = name;
     }
 
-    @Override
-    protected void handleNormalizedLoggingCall(Level level, Marker marker, String format, Object[] args, Throwable cause) {
-        String logMessage = prepareFormattedMessage(format, args);
-        LogEntry entry = new LogEntry(logMessage, cause, new Date(), level);
-
-        if (level == Level.ERROR && !isFinalizing) {
-            allErrors.add(entry);
-        } else {
-            printLogEntry(entry);
-        }
-    }
-
-    private String prepareFormattedMessage(String format, Object[] args) {
-        if (args == null) {
-            return format;
-        }
-
-        return MessageFormatter.arrayFormat(format, args).getMessage();
-    }
-
-    private void printLogEntry(LogEntry entry) {
-        StringBuilder output = prepareLogOutput(entry);
-
-        TARGET_STREAM.println(output);
-        if (entry.cause() != null) {
-            entry.cause().printStackTrace(TARGET_STREAM);
-        }
-        TARGET_STREAM.flush();
-    }
-
-    private StringBuilder prepareLogOutput(LogEntry entry) {
-        StringBuilder outputBuilder = new StringBuilder(MAXIMUM_MESSAGE_LENGTH);
-        outputBuilder.append(dateFormat.format(entry.timeOfLog())).append(' ');
-        outputBuilder.append('[').append(entry.logLevel().name()).append("] ");
-        outputBuilder.append(computeShortName()).append(" - ");
-        outputBuilder.append(entry.message());
-        return outputBuilder;
-    }
-
-    void printAllErrorsForLogger() {
-        isFinalizing = true;
-        ArrayList<LogEntry> errors = new ArrayList<>(allErrors);
-
-        if (!errors.isEmpty()) {
-            info("Summary of all errors:");
-            allErrors.clear();
-            errors.forEach(this::printLogEntry);
-        }
-
-        isFinalizing = false;
-    }
-
-    private String computeShortName() {
-        return name.substring(name.lastIndexOf(".") + 1);
+    /**
+     * Creates a logger with a specific name and {@link Level.INFO}.
+     * @param currentLogLevel is the current log level.
+     * @param name is the name of the logger.
+     */
+    CollectedLogger(String name) {
+        this(Level.INFO, name);
     }
 
     @Override
@@ -161,16 +100,69 @@ public class CollectedLogger extends AbstractLogger {
         return isErrorEnabled();
     }
 
-    private boolean isLogLevelEnabled(Level logLevel) {
-        return logLevel.toInt() >= (isJPlagLog() ? currentLogLevel.toInt() : LOG_LEVEL_FOR_EXTERNAL_LIBRARIES.toInt());
+    @Override
+    protected String getFullyQualifiedCallerName() {
+        return null; // does not seem to be used by anything, but is required by SLF4J
+    }
+
+    @Override
+    protected void handleNormalizedLoggingCall(Level level, Marker marker, String format, Object[] args, Throwable cause) {
+        String logMessage = prepareFormattedMessage(format, args);
+        LogEntry entry = new LogEntry(logMessage, cause, new Date(), level);
+
+        if (level == Level.ERROR && !isFinalizing) {
+            allErrors.add(entry);
+        } else {
+            printLogEntry(entry);
+        }
+    }
+
+    void printAllErrorsForLogger() {
+        isFinalizing = true;
+        ArrayList<LogEntry> errors = new ArrayList<>(allErrors);
+        if (!errors.isEmpty()) {
+            info("Summary of all errors:");
+            allErrors.clear();
+            errors.forEach(this::printLogEntry);
+        }
+        isFinalizing = false;
+    }
+
+    private String computeShortName() {
+        return name.substring(name.lastIndexOf(".") + 1);
     }
 
     private boolean isJPlagLog() {
         return name.startsWith(JPLAG_LOGGER_PREFIX);
     }
 
-    @Override
-    protected String getFullyQualifiedCallerName() {
-        return null; // does not seem to be used by anything, but is required by SLF4J
+    private boolean isLogLevelEnabled(Level logLevel) {
+        return logLevel.toInt() >= (isJPlagLog() ? currentLogLevel.toInt() : LOG_LEVEL_FOR_EXTERNAL_LIBRARIES.toInt());
+    }
+
+    private String prepareFormattedMessage(String format, Object[] args) {
+        if (args == null) {
+            return format;
+        }
+
+        return MessageFormatter.arrayFormat(format, args).getMessage();
+    }
+
+    private StringBuilder prepareLogOutput(LogEntry entry) {
+        StringBuilder outputBuilder = new StringBuilder(MAXIMUM_MESSAGE_LENGTH);
+        outputBuilder.append(dateFormat.format(entry.timeOfLog())).append(' ');
+        outputBuilder.append('[').append(entry.logLevel().name()).append("] ");
+        outputBuilder.append(computeShortName()).append(" - ");
+        outputBuilder.append(entry.message());
+        return outputBuilder;
+    }
+
+    private void printLogEntry(LogEntry entry) {
+        StringBuilder output = prepareLogOutput(entry);
+        TARGET_STREAM.println(output);
+        if (entry.cause() != null) {
+            entry.cause().printStackTrace(TARGET_STREAM);
+        }
+        TARGET_STREAM.flush();
     }
 }
