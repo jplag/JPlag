43eb2bad85aa000e797d13809e146a67d7efe2d3
Alexander Vogt <alexander_vogt@outlook.de>
2024-07-30 10:43:40 +0200

    make colors lighter

--- report-viewer/tailwind.config.js
+++ report-viewer/tailwind.config.js
@@ -13,26 +13,26 @@ export default {
         },
         background: {
           light: 'hsl(0, 0%, 97%)',
-          dark: 'hsl(180, 80%, 3%)'
+          dark: 'hsl(180, 80%, 8%)'
         },
         container: {
           light: 'hsl(0, 0%, 98%)',
-          dark: 'hsl(200, 20%, 13%)',
+          dark: 'hsl(200, 20%, 15%)',
           border: {
             light: 'hsl(0, 0%, 80%)',
             dark: 'hsl(0, 0%, 25%)'
           },
           secondary: {
             light: 'hsl(0, 0%, 95%)',
-            dark: 'hsl(200, 20%, 18%)'
+            dark: 'hsl(200, 20%, 20%)'
           }
         },
         interactable: {
           light: 'hsl(0, 0%, 100%)',
-          dark: 'hsl(180, 30%, 18%)',
+          dark: 'hsl(180, 30%, 20%)',
           border: {
             light: 'hsl(0, 0%, 75%)',
-            dark: 'hsl(0, 0%, 30%)'
+            dark: 'hsl(0, 0%, 25%)'
           }
         },
         scrollbar: {
