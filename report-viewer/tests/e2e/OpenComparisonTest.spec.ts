4fcf51f7ed86b0771773e8e9667fca0d642e78a2
euberdeveloper <euberdeveloper@gmail.com>
2024-09-02 23:04:29 +0200

    test(viewer): fix e2e regexp

--- report-viewer/tests/e2e/OpenComparisonTest.spec.ts
+++ report-viewer/tests/e2e/OpenComparisonTest.spec.ts
@@ -48,8 +48,8 @@ const testSets: DataSet[] = [
   },
   {
     datasetName: 'submissionsWithSameName-report.zip',
-    firstSubmissionName: `2020${regexpPathSeparator}gr1`,
-    secondSubmissionName: `2020${regexpPathSeparator}gr2`
+    firstSubmissionName: `2023${regexpPathSeparator}gr1`,
+    secondSubmissionName: `2023${regexpPathSeparator}gr2`
   },
   {
     datasetName: 'python-report.zip',
