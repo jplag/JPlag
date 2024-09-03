918c7093f27aae6cba8056e9363242b62a3b3aa8
Alexander Milster <alexander.milster@gmx.de>
2024-09-03 15:33:57 +0200

    Added token position tests for annotations

---
+++ languages/java/src/test/resources/de/jplag/java/tokenPositions/JAnnoTBegin-End_1.java
@@ -0,0 +1,3 @@
+>public @interface MyAnnotation {}
+$| J_ANNO_T_BEGIN 30
+$                                | J_ANNO_T_END 1
---
+++ languages/java/src/test/resources/de/jplag/java/tokenPositions/JAnnoTBegin-End_2.java
@@ -0,0 +1,4 @@
+>public @interface MyAnnotation {
+$| J_ANNO_T_BEGIN 30
+>}
+$| J_ANNO_T_END 1
