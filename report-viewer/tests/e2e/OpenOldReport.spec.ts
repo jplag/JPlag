34dcbbe84ff107ea249e623e01e28b0d93ea0a2a
Alex | Kronox <alexander_vogt@outlook.de>
2024-11-30 13:04:54 +0100

    add tests

---
+++ report-viewer/tests/e2e/OpenOldReport.spec.ts
@@ -0,0 +1,32 @@
+import { expect, test } from '@playwright/test'
+import { uploadFile } from './TestUtils'
+
+const oldVersionZips = [
+  {
+    zipName: 'progpedia-report-v5_1_0.zip',
+    version: '5.1.0',
+    urlPostfix: 'v5'
+  },
+  {
+    zipName: 'progpedia-report-v4_2_0.zip',
+    version: '4.2.0',
+    urlPostfix: 'v5'
+  }
+]
+
+for (const oldVersion of oldVersionZips) {
+  test(`Test old version redirect for v${oldVersion.version}`, async ({ page }) => {
+    await page.goto('/')
+    await uploadFile(oldVersion.zipName, page, '/old/' + oldVersion.version)
+
+    const bodyContent = await page.locator('body').textContent()
+    expect(bodyContent).toContain('You are trying to open a report from an older version of JPlag')
+    expect(bodyContent).toContain(oldVersion.version)
+    expect(bodyContent).toContain('You can access the old report viewer here:')
+    expect(bodyContent).toContain('Open old report viewer')
+
+    const oldVersionLinkElement = await page.locator('a').first()
+    const oldVersionURL = await oldVersionLinkElement.getAttribute('href')
+    expect(oldVersionURL.endsWith('/v5/')).toBeTruthy()
+  })
+}
