eab9c97c32d38e5b55b6a611e7096d9226a41eb0
Alex | Kronox <alexander_vogt@outlook.de>
2024-12-01 07:54:26 +0100

    format config

--- report-viewer/eslint.config.mjs
+++ report-viewer/eslint.config.mjs
@@ -1,19 +1,16 @@
 import pluginVue from 'eslint-plugin-vue'
 import js from '@eslint/js'
-import tseslint from 'typescript-eslint';
+import tseslint from 'typescript-eslint'
 import vueParser from 'vue-eslint-parser'
-import eslintConfigPrettier from "eslint-config-prettier";
-//import vueTypescript from '@vue/eslint-config-typescript'
-//import vueSkipFormatting, { ignores } from '@vue/eslint-config-prettier/skip-formatting'
-//import vue3Essential from 'eslint-plugin-vue/lib/configs/vue3-essential.js'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default [
-   js.configs.recommended,
-   ...pluginVue.configs['flat/recommended'],
+  js.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
   ...tseslint.configs.recommended,
   eslintConfigPrettier,
   {
-    ignores: ['**/*.config.ts', 'node_modules/**' , 'dist/**', '**/playwright-report/**'],
+    ignores: ['**/*.config.ts', 'node_modules/**', 'dist/**', '**/playwright-report/**']
   },
   {
     files: ['**/*.js', '**/*.ts', '**/*.tsx', '**/*.vue'],
@@ -28,19 +25,19 @@ export default [
     rules: {
       'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
       'no-restricted-exports': ['error', { restrictDefaultExports: { direct: true } }],
-      'vue/no-setup-props-reactivity-loss': 'error',
-    },
+      'vue/no-setup-props-reactivity-loss': 'error'
+    }
   },
   {
     files: ['**/*.config.ts', '**/*.config.js', '**/*.d.ts'],
     rules: {
-      'no-restricted-exports': 'off',
-    },
+      'no-restricted-exports': 'off'
+    }
   },
   {
     files: ['**/*.test.ts'],
     rules: {
-        '@typescript-eslint/no-unused-expressions': 'off'
+      '@typescript-eslint/no-unused-expressions': 'off'
     }
   }
 ]
