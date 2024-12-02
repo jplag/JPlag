d62a35856ae646424ebb28621d8c198b3e5505ca
Alexander Milster <alexander.milster@gmx.de>
2024-12-02 10:47:12 +0100

    Added simple tests for multi language

--- languages/multi-language/src/main/java/de/jplag/multilang/MultiLanguageOptions.java
+++ languages/multi-language/src/main/java/de/jplag/multilang/MultiLanguageOptions.java
@@ -11,20 +11,24 @@ import de.jplag.options.OptionType;
 
 public class MultiLanguageOptions extends LanguageOptions {
     private static final String ERROR_LANGUAGE_NOT_FOUND = "The selected language %s could not be found";
-    private static final String ERROR_NOT_ENOUGH_LANGUAGES = "To use multi language specify at least 2 languages";
+    private static final String ERROR_NOT_ENOUGH_LANGUAGES = "To use multi language specify at least 1 language";
 
-    public LanguageOption<String> languageNames = createOption(OptionType.string(), "langs",
+    public LanguageOption<String> languageNames = createOption(OptionType.string(), "languages",
             "The languages that should be used. This is a ',' separated list");
     private List<Language> languages = null;
 
     public List<Language> getLanguages() {
         if (this.languages == null) {
+            if (languageNames.getValue() == null) {
+                throw new IllegalArgumentException(ERROR_NOT_ENOUGH_LANGUAGES);
+            }
+
             List<Language> languages = Arrays.stream(languageNames.getValue().split(","))
                     .map(name -> LanguageLoader.getLanguage(name)
                             .orElseThrow(() -> new IllegalArgumentException(String.format(ERROR_LANGUAGE_NOT_FOUND, name))))
                     .filter(language -> !language.getClass().equals(MultiLanguage.class)).toList();
 
-            if (languages.size() <= 1) {
+            if (languages.isEmpty()) {
                 throw new IllegalArgumentException(ERROR_NOT_ENOUGH_LANGUAGES);
             }
 
