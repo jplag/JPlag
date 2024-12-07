0ec99b0d592ffc332d7c7ab8a47aaf0dd9e3a344
Alex | Kronox <alexander_vogt@outlook.de>
2024-12-07 19:47:05 +0100

    disable no unused rule for one line

--- report-viewer/src/model/factories/BaseFactory.ts
+++ report-viewer/src/model/factories/BaseFactory.ts
@@ -66,9 +66,10 @@ export class BaseFactory {
     try {
       await this.getLocalFile(this.zipFileName)
       return true
+      /* eslint-disable @typescript-eslint/no-unused-vars */
     } catch (e) {
-      void e
       return false
     }
+    /* eslint-enable @typescript-eslint/no-unused-vars */
   }
 }
