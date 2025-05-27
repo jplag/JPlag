041e8120fcc4d8e2d7c522b763b0037fad8b6300
Timur Saglam <timur.saglam@kit.edu>
2025-05-27 08:39:29 +0200

    Adapt similarity calculation to reflect asymmetric matches.

--- core/src/main/java/de/jplag/JPlagComparison.java
+++ core/src/main/java/de/jplag/JPlagComparison.java
@@ -12,6 +12,7 @@ import java.util.List;
  * threshold.
  */
 public record JPlagComparison(Submission firstSubmission, Submission secondSubmission, List<Match> matches, List<Match> ignoredMatches) {
+
     /**
      * Constructs a new comparison between two submissions. The match lists are wrapped as unmodifiable to preserve
      * immutability.
@@ -61,9 +62,13 @@ public record JPlagComparison(Submission firstSubmission, Submission secondSubmi
      * structural similarity.
      */
     public final double similarity() {
-        int divisorA = firstSubmission.getSimilarityDivisor();
-        int divisorB = secondSubmission.getSimilarityDivisor();
-        return 2 * similarity(divisorA + divisorB);
+        int divisor = firstSubmission.getSimilarityDivisor() + secondSubmission.getSimilarityDivisor();
+        if (divisor == 0) {
+            return 0;
+        }
+        int matchedTokensOfFirst = matches.stream().mapToInt(Match::lengthOfFirst).sum();
+        int matchedTokensOfSecond = matches.stream().mapToInt(Match::lengthOfSecond).sum();
+        return (matchedTokensOfFirst + matchedTokensOfSecond) / (double) divisor;
     }
 
     /**
@@ -73,7 +78,8 @@ public record JPlagComparison(Submission firstSubmission, Submission secondSubmi
      */
     public final double similarityOfFirst() {
         int divisor = firstSubmission.getSimilarityDivisor();
-        return similarity(divisor);
+        int matchedTokens = matches.stream().mapToInt(Match::lengthOfFirst).sum();
+        return divisor == 0 ? 0.0 : matchedTokens / (double) divisor;
     }
 
     /**
@@ -83,7 +89,8 @@ public record JPlagComparison(Submission firstSubmission, Submission secondSubmi
      */
     public final double similarityOfSecond() {
         int divisor = secondSubmission.getSimilarityDivisor();
-        return similarity(divisor);
+        int matchedTokens = matches.stream().mapToInt(Match::lengthOfSecond).sum();
+        return divisor == 0 ? 0.0 : matchedTokens / (double) divisor;
     }
 
     @Override
@@ -91,7 +98,4 @@ public record JPlagComparison(Submission firstSubmission, Submission secondSubmi
         return firstSubmission.getName() + " <-> " + secondSubmission.getName();
     }
 
-    private double similarity(int divisor) {
-        return divisor == 0 ? 0.0 : getNumberOfMatchedTokens() / (double) divisor;
-    }
 }
