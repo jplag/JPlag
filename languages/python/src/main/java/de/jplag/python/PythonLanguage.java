7516f39ccec79efe3a737c219de348e4fa7aa736
Bjoern Holtvogt <bjoern.holtvogt@gmail.com>
2025-05-28 16:34:29 +0200

    Add Python language class

---
+++ languages/python/src/main/java/de/jplag/python/PythonLanguage.java
@@ -0,0 +1,40 @@
+package de.jplag.python;
+
+import java.io.File;
+import java.util.List;
+import java.util.Set;
+
+import org.kohsuke.MetaInfServices;
+
+import de.jplag.Language;
+import de.jplag.ParsingException;
+import de.jplag.Token;
+
+@MetaInfServices(Language.class)
+public class PythonLanguage implements Language {
+
+    @Override
+    public String[] suffixes() {
+        return new String[] {".py"};
+    }
+
+    @Override
+    public String getName() {
+        return "Python";
+    }
+
+    @Override
+    public String getIdentifier() {
+        return "python3";
+    }
+
+    @Override
+    public int minimumTokenMatch() {
+        return 12;
+    }
+
+    @Override
+    public List<Token> parse(Set<File> files, boolean normalize) throws ParsingException {
+        return new PythonParserAdapter().parse(files);
+    }
+}
