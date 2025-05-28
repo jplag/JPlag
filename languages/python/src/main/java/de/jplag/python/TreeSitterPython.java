a96645785398c6085a42a5e4b675dd5cea03734c
Bjoern Holtvogt <bjoern.holtvogt@gmail.com>
2025-05-28 15:29:04 +0200

    Add Tree-sitter-based Python module

---
+++ languages/python/src/main/java/de/jplag/python/TreeSitterPython.java
@@ -0,0 +1,27 @@
+package de.jplag.python;
+
+import java.lang.foreign.MemorySegment;
+
+import de.jplag.treesitter.TreeSitterLanguage;
+import de.jplag.treesitter.library.NativeLibraryType;
+
+public class TreeSitterPython extends TreeSitterLanguage {
+    private static final TreeSitterPython INSTANCE = new TreeSitterPython();
+
+    private TreeSitterPython() {
+    }
+
+    public static MemorySegment language() {
+        return INSTANCE.call();
+    }
+
+    @Override
+    protected NativeLibraryType libraryType() {
+        return NativeLibraryType.TREE_SITTER_PYTHON;
+    }
+
+    @Override
+    protected String symbolName() {
+        return "tree_sitter_python";
+    }
+}
---
+++ languages/python/src/test/java/de/jplag/python/TreeSitterPythonTest.java
@@ -0,0 +1,25 @@
+package de.jplag.python;
+
+import org.junit.jupiter.api.Test;
+
+import io.github.treesitter.jtreesitter.InputEncoding;
+import io.github.treesitter.jtreesitter.Language;
+import io.github.treesitter.jtreesitter.Node;
+import io.github.treesitter.jtreesitter.Parser;
+import io.github.treesitter.jtreesitter.Tree;
+
+public class TreeSitterPythonTest {
+    @Test
+    public void testTreeSitterPythonIntegration() {
+        Language language = new Language(TreeSitterPython.language());
+        try (Parser parser = new Parser(language)) {
+            try (Tree tree = parser.parse("def main()", InputEncoding.UTF_8).orElseThrow()) {
+                Node rootNode = tree.getRootNode();
+                System.out.println("Root Node: " + rootNode.getType());
+                assert rootNode.getType().equals("module");
+                assert rootNode.getStartPoint().column() == 0;
+                assert rootNode.getEndPoint().column() == 10;
+            }
+        }
+    }
+}
