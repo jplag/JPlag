2357398aaac69d176d7ae2a5b0d53af057e59120
Moritz R <c0derMo@users.noreply.github.com>
2025-06-05 13:01:33 +0200

    Testing the comment extractor

---
+++ language-api/src/test/java/de/jplag/CommentExtractorTest.java
@@ -0,0 +1,59 @@
+package de.jplag;
+
+import de.jplag.commentextraction.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.StringJoiner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CommentExtractorTest {
+
+    private static final Path TEST_FILE_LOCATION = Path.of("src", "test", "resources", "de", "jplag", "samples");
+    private static final String TEST_FILE_NAME = "CommentExtractorTest.txt";
+
+    @Test
+    void testCommentExtractor() throws IOException {
+        CommentExtractorSettings settings = new CommentExtractorSettings(
+                List.of(new EnvironmentDelimiter("\"")), // No comment environments
+                List.of("//"), // Line comments
+                List.of(new EnvironmentDelimiter("/*", "*/")), // Block comments
+                List.of("\\") // Escape characters
+        );
+
+        File input = new File(TEST_FILE_LOCATION.toFile(), TEST_FILE_NAME);
+
+        CommentExtractor extractor = new CommentExtractor(input, settings);
+
+        List<Comment> comments = extractor.extract();
+
+        assertEquals(2, comments.size());
+        assertEquals(new Comment(
+                input,
+                " This is a line comment.",
+                2,
+                3,
+                CommentType.LINE
+        ), comments.get(0));
+
+        StringJoiner multilineComment = new StringJoiner(System.lineSeparator());
+        multilineComment.add(" This is");
+        multilineComment.add("a");
+        multilineComment.add("multiline");
+        multilineComment.add("comment");
+        multilineComment.add("");
+
+        assertEquals(new Comment(
+                input,
+                multilineComment.toString(),
+                4,
+                3,
+                CommentType.BLOCK
+        ), comments.get(1));
+    }
+
+}
