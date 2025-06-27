aded5e01a9c8049eb1aa5b574f702f204eaa984d
heylissi <e2.hermann@gmail.com>
2025-06-27 13:15:15 +0200

    deleted FrequencyStrategyfactory and added logic to FrequencyStrategies

--- core/src/main/java/de/jplag/highlight_extraction/FrequencyStrategies.java
+++ core/src/main/java/de/jplag/highlight_extraction/FrequencyStrategies.java
@@ -4,8 +4,18 @@ package de.jplag.highlight_extraction;
  * Enum representing the different strategies for frequency calculation of token subsequences in comparisons.
  */
 public enum FrequencyStrategies {
-    COMPLETE_MATCHES,
-    CONTAINED_MATCHES,
-    SUB_MATCHES,
-    WINDOW_OF_MATCHES;
+    COMPLETE_MATCHES(new CompleteMatchesStrategy()),
+    CONTAINED_MATCHES(new ContainedMatchesStrategy()),
+    SUB_MATCHES(new SubMatchesStrategy()),
+    WINDOW_OF_MATCHES(new WindowOfMatchesStrategy());
+
+    private final FrequencyStrategy strategy;
+
+    FrequencyStrategies(FrequencyStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public FrequencyStrategy getStrategy() {
+        return strategy;
+    }
 }
