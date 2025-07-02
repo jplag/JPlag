d3df48ddd0ad725c62ceede37f18e4e59b3d8a91
Moritz R <c0derMo@users.noreply.github.com>
2025-07-02 19:17:17 +0200

    Simplify comment comparer, implement comparison flipping

--- core/src/main/java/de/jplag/commenthandling/CommentComparer.java
+++ core/src/main/java/de/jplag/commenthandling/CommentComparer.java
@@ -1,23 +1,14 @@
 package de.jplag.commenthandling;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Collections;
+import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import de.jplag.JPlagComparison;
-import de.jplag.JPlagResult;
-import de.jplag.Submission;
-import de.jplag.SubmissionSet;
+import de.jplag.*;
 import de.jplag.comparison.GreedyStringTiling;
-import de.jplag.comparison.SubmissionTuple;
 import de.jplag.comparison.TokenValueMapper;
 import de.jplag.options.JPlagOptions;
 
 public class CommentComparer {
-    private final Logger logger = LoggerFactory.getLogger(CommentComparer.class);
-
     private final JPlagOptions options;
 
     public CommentComparer(JPlagOptions options) {
@@ -43,19 +34,14 @@ public class CommentComparer {
         }
     }
 
-    private Optional<JPlagComparison> compareSubmissions(GreedyStringTiling algorithm, SubmissionTuple tuple) {
-        JPlagComparison comp = algorithm.compare(tuple.left(), tuple.right());
-        return Optional.of(comp);
+    public List<Match> flipMatches(List<Match> matches) {
+        return matches.stream().map(match -> new Match(match.startOfSecond(), match.startOfFirst(), match.endOfSecond(), match.endOfFirst()))
+                .toList();
     }
 
-    private List<SubmissionTuple> extractComparisonTuples(List<JPlagComparison> comparisons) {
-        List<SubmissionTuple> tuples = new ArrayList<>();
-
-        for (JPlagComparison comparison : comparisons) {
-            tuples.add(new SubmissionTuple(comparison.firstSubmission(), comparison.secondSubmission()));
-        }
-
-        return tuples;
+    public JPlagComparison flipComparison(JPlagComparison comparison) {
+        return new JPlagComparison(comparison.secondSubmission(), comparison.firstSubmission(), this.flipMatches(comparison.matches()),
+                this.flipMatches(comparison.ignoredMatches()), this.flipMatches(comparison.commentMatches()));
     }
 
     public JPlagResult compareCommentsAndMergeMatches(JPlagResult result) {
@@ -69,26 +55,16 @@ public class CommentComparer {
             compareSubmissionsToBaseCode(result.getSubmissions(), algorithm);
         }
 
-        List<SubmissionTuple> tuples = this.extractComparisonTuples(result.getAllComparisons());
-
-        Map<SubmissionTuple, JPlagComparison> commentComparisons = tuples.stream().parallel().flatMap(tuple -> {
-            return this.compareSubmissions(algorithm, tuple).stream().map(comparison -> new AbstractMap.SimpleEntry<>(tuple, comparison));
-        }).collect(Collectors.toMap(AbstractMap.SimpleEntry::getKey, AbstractMap.SimpleEntry::getValue));
-
-        List<JPlagComparison> fixedComparisons = new ArrayList<>();
-
-        for (JPlagComparison oldComparison : result.getAllComparisons()) {
-            JPlagComparison commentComparison = commentComparisons
-                    .get(new SubmissionTuple(oldComparison.firstSubmission(), oldComparison.secondSubmission()));
-            if (commentComparison == null) {
-                logger.warn("No comment comparison found for: {}", oldComparison);
-                fixedComparisons.add(oldComparison);
-                continue;
+        List<JPlagComparison> fixedComparisons = result.getAllComparisons().stream().parallel().map(oldComparison -> {
+            JPlagComparison commentComparison = algorithm.compare(oldComparison.firstSubmission(), oldComparison.secondSubmission());
+            if (commentComparison.firstSubmission().equals(oldComparison.secondSubmission())) {
+                // Due to internals of the comparison algorithm, the submissions are flipped between code & comment comparisons
+                // To ensure that matches are assigned to the correct submission, we're flipping the comment comparison
+                commentComparison = this.flipComparison(commentComparison);
             }
-
-            fixedComparisons.add(new JPlagComparison(oldComparison.firstSubmission(), oldComparison.secondSubmission(), oldComparison.matches(),
-                    oldComparison.ignoredMatches(), commentComparison.matches()));
-        }
+            return new JPlagComparison(oldComparison.firstSubmission(), oldComparison.secondSubmission(), oldComparison.matches(),
+                    oldComparison.ignoredMatches(), commentComparison.matches());
+        }).toList();
 
         long durationInMillis = System.currentTimeMillis() - timeBeforeStartInMillis;
 
