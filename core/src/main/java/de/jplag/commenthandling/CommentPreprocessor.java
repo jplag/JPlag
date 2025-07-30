99c4631721dc17f888c06fd4f42e976843c2fb1d
Moritz R <c0derMo@users.noreply.github.com>
2025-07-30 14:25:18 +0200

    Simplify comment preprocessor

--- core/src/main/java/de/jplag/commenthandling/CommentPreprocessor.java
+++ core/src/main/java/de/jplag/commenthandling/CommentPreprocessor.java
@@ -19,8 +19,7 @@ public class CommentPreprocessor {
     private static final Logger logger = LoggerFactory.getLogger(CommentPreprocessor.class);
 
     private final List<Comment> comments;
-    private final Map<Integer, Comment> lineToComment;
-    private final Map<Comment, Integer> commentStartingLines;
+    private final ParserAdapter textParser;
 
     /**
      * Creates a new preprocessor for the supplied list of comments.
@@ -28,8 +27,8 @@ public class CommentPreprocessor {
      */
     public CommentPreprocessor(List<Comment> comments) {
         this.comments = comments;
-        this.lineToComment = new HashMap<>();
-        this.commentStartingLines = new HashMap<>();
+        this.comments.sort(Comparator.comparing(Comment::file));
+        this.textParser = new ParserAdapter();
     }
 
     /**
@@ -37,62 +36,45 @@ public class CommentPreprocessor {
      * @return List of tokens containing all comments
      */
     public List<Token> processToToken() {
-        ParserAdapter textParser = new ParserAdapter();
+        List<Token> result = new ArrayList<>();
+        File lastFile = null;
+        for (Comment comment : comments) {
+            result.addAll(processSingleCommentToToken(comment));
+            if (comment.file() != lastFile) {
+                if (lastFile != null) {
+                    result.add(Token.fileEnd(lastFile));
+                }
+                lastFile = comment.file();
+            }
+        }
+        result.add(Token.fileEnd(lastFile));
+        return result;
+    }
+
+    private List<Token> processSingleCommentToToken(Comment comment) {
         try {
-            return fixTokenPositions(textParser.parseStrings(Set.of(this.buildCommentString())));
+            return fixTokenPositions(textParser.parseStrings(Set.of(comment.content())), comment);
         } catch (ParsingException e) {
-            logger.error("Could not parse comments: {}", e.getMessage());
+            logger.error("Could not parse comment: {}", e.getMessage());
             return List.of();
         }
     }
 
-    private List<Token> fixTokenPositions(List<Token> tokens) {
+    private List<Token> fixTokenPositions(List<Token> tokens, Comment comment) {
         List<Token> fixedTokens = new ArrayList<>();
-        File lastFile = null;
         for (Token token : tokens) {
             if (token.getType() == SharedTokenType.FILE_END) {
-                fixedTokens.add(Token.fileEnd(lastFile));
                 continue;
             }
 
-            Comment originalComment = this.lineToComment.get(token.getLine());
-            if (originalComment == null) {
-                logger.warn("Original comment not found for token {}!", token);
-                fixedTokens.add(token);
-                continue;
-            }
-
-            if (lastFile != null && !lastFile.equals(originalComment.file())) {
-                fixedTokens.add(Token.fileEnd(lastFile));
-            }
-            lastFile = originalComment.file();
-            int line = originalComment.line() + token.getLine() - commentStartingLines.get(originalComment);
+            int line = comment.line() + token.getLine() - 1;
             int column = token.getColumn();
-
-            if (commentStartingLines.get(originalComment) == token.getLine()) {
-                column += originalComment.column() - 1;
+            if (token.getLine() == 1) {
+                column += comment.column() - 1;
             }
 
-            Token newToken = new Token(token.getType(), originalComment.file(), line, column, token.getLength(), token.getSemantics());
-            fixedTokens.add(newToken);
+            fixedTokens.add(new Token(token.getType(), comment.file(), line, column, token.getLength()));
         }
         return fixedTokens;
-    }
-
-    private String buildCommentString() {
-        this.commentStartingLines.clear();
-        this.lineToComment.clear();
-        StringJoiner joiner = new StringJoiner(System.lineSeparator());
-        int line = 1;
-        for (Comment comment : comments) {
-            int lines = comment.content().split(System.lineSeparator(), -1).length;
-            commentStartingLines.put(comment, line);
-            for (int i = 0; i < lines; i++) {
-                lineToComment.put(line + i, comment);
-            }
-            joiner.add(comment.content());
-            line += lines;
-        }
-        return joiner.toString();
     }
 }
