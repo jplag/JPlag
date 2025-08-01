bb81fff62178c7e099745f8c1bd47d1d2c5e9c46
Timur Saglam <timur.saglam@kit.edu>
2025-08-01 10:55:41 +0200

    Fix formatting.

--- core/src/main/java/de/jplag/reporting/reportobject/model/SubmissionFile.java
+++ core/src/main/java/de/jplag/reporting/reportobject/model/SubmissionFile.java
@@ -2,8 +2,7 @@ package de.jplag.reporting.reportobject.model;
 
 /**
  * Represents metadata about a submission file used in a plagiarism comparison.
- * @param tokenCount The number of tokens contained in the file.
- * of tokenized elements.
+ * @param tokenCount The number of tokens contained in the file. of tokenized elements.
  */
 public record SubmissionFile(int tokenCount) {
 }
