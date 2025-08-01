cd7996199943acdfbbb57fd35c2bd47d68990736
Timur Sağlam <timur.saglam@kit.edu>
2025-08-01 10:55:06 +0200

    Resolve issues in report creation JDoc comments.
    Co-authored-by: Alex | Kronox <39801116+Kr0nox@users.noreply.github.com>

--- core/src/main/java/de/jplag/reporting/reportobject/model/SubmissionFile.java
+++ core/src/main/java/de/jplag/reporting/reportobject/model/SubmissionFile.java
@@ -2,7 +2,7 @@ package de.jplag.reporting.reportobject.model;
 
 /**
  * Represents metadata about a submission file used in a plagiarism comparison.
- * @param tokenCount The number of tokens contained in the file. This indicates the file's size or complexity in terms
+ * @param tokenCount The number of tokens contained in the file.
  * of tokenized elements.
  */
 public record SubmissionFile(int tokenCount) {
--- core/src/main/java/de/jplag/reporting/reportobject/model/SubmissionMappings.java
+++ core/src/main/java/de/jplag/reporting/reportobject/model/SubmissionMappings.java
@@ -4,9 +4,9 @@ import java.util.Map;
 
 /**
  * Holds mappings related to submissions and their associated comparison file names.
- * @param submissionIds A map from submission identifiers to their internal or normalized submission IDs.
+ * @param submissionIds A map from a submission ID to the submissions display name.
  * @param submissionIdsToComparisonFileName A nested map where each key is a submission ID, and the value is another map
- * that associates comparison file names with their normalized or processed names.
+ * that maps a second submission ID to the name of the JSON file containing the comparison of the two submissions.
  */
 public record SubmissionMappings(Map<String, String> submissionIds, Map<String, Map<String, String>> submissionIdsToComparisonFileName) {
 }
