680224b3be2ecad0f3170808825d164871fddfe8
Bjoern Holtvogt <bjoern.holtvogt@gmail.com>
2025-08-02 11:31:44 +0200

    Implement token extraction in PythonParserAdapter

--- languages/python/src/main/java/de/jplag/python/PythonParserAdapter.java
+++ languages/python/src/main/java/de/jplag/python/PythonParserAdapter.java
@@ -1,11 +1,14 @@
 package de.jplag.python;
 
 import java.io.File;
+import java.io.IOException;
 import java.lang.foreign.MemorySegment;
+import java.nio.file.Files;
 import java.util.List;
 
 import de.jplag.Token;
 import de.jplag.treesitter.AbstractTreeSitterParserAdapter;
+import de.jplag.treesitter.TreeSitterTraversal;
 
 import io.github.treesitter.jtreesitter.Node;
 
@@ -18,6 +21,16 @@ public class PythonParserAdapter extends AbstractTreeSitterParserAdapter {
 
     @Override
     protected List<Token> extractTokens(File file, Node rootNode) {
-        throw new UnsupportedOperationException("Unimplemented method 'extractTokens'");
+        try {
+            // TODO: Consider caching file content to avoid re-reading
+            String code = Files.readString(file.toPath());
+            PythonTokenCollector collector = new PythonTokenCollector(file, code);
+            TreeSitterTraversal.traverse(rootNode, collector);
+            List<Token> tokens = collector.getTokens();
+            tokens.add(Token.fileEnd(file));
+            return tokens;
+        } catch (IOException exception) {
+            throw new RuntimeException("Failed to read file: " + file.getName(), exception);
+        }
     }
 }
