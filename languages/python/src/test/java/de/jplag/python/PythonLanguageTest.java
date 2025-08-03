77cfcabde5b477bb16dc9750f7c48de7997629cb
Bjoern Holtvogt <bjoern.holtvogt@gmail.com>
2025-08-03 12:50:36 +0200

    Update language test with list, set and dict tokens

--- languages/python/src/test/java/de/jplag/python/PythonLanguageTest.java
+++ languages/python/src/test/java/de/jplag/python/PythonLanguageTest.java
@@ -1,7 +1,6 @@
 package de.jplag.python;
 
 import static de.jplag.python.PythonTokenType.APPLY;
-import static de.jplag.python.PythonTokenType.ASSERT;
 import static de.jplag.python.PythonTokenType.ASSIGN;
 import static de.jplag.python.PythonTokenType.ASYNC;
 import static de.jplag.python.PythonTokenType.AWAIT;
@@ -10,24 +9,23 @@ import static de.jplag.python.PythonTokenType.CASE;
 import static de.jplag.python.PythonTokenType.CLASS_BEGIN;
 import static de.jplag.python.PythonTokenType.CLASS_END;
 import static de.jplag.python.PythonTokenType.CONTINUE;
-import static de.jplag.python.PythonTokenType.DEL;
+import static de.jplag.python.PythonTokenType.DICTIONARY;
 import static de.jplag.python.PythonTokenType.EXCEPT_BEGIN;
 import static de.jplag.python.PythonTokenType.EXCEPT_END;
 import static de.jplag.python.PythonTokenType.FINALLY_BEGIN;
 import static de.jplag.python.PythonTokenType.FINALLY_END;
 import static de.jplag.python.PythonTokenType.FOR_BEGIN;
 import static de.jplag.python.PythonTokenType.FOR_END;
-import static de.jplag.python.PythonTokenType.GLOBAL;
 import static de.jplag.python.PythonTokenType.IF_BEGIN;
 import static de.jplag.python.PythonTokenType.IF_END;
 import static de.jplag.python.PythonTokenType.IMPORT;
 import static de.jplag.python.PythonTokenType.LAMBDA;
+import static de.jplag.python.PythonTokenType.LIST;
 import static de.jplag.python.PythonTokenType.MATCH_BEGIN;
 import static de.jplag.python.PythonTokenType.MATCH_END;
 import static de.jplag.python.PythonTokenType.METHOD_BEGIN;
 import static de.jplag.python.PythonTokenType.METHOD_END;
 import static de.jplag.python.PythonTokenType.NAMED_EXPR;
-import static de.jplag.python.PythonTokenType.NONLOCAL;
 import static de.jplag.python.PythonTokenType.PASS;
 import static de.jplag.python.PythonTokenType.RAISE;
 import static de.jplag.python.PythonTokenType.RETURN;
@@ -54,12 +52,16 @@ public class PythonLanguageTest extends LanguageModuleTest {
 
     @Override
     protected void collectTestData(TestDataCollector collector) {
-        collector.testFile("BasicTest.py").testSourceCoverage().testTokenSequence(IMPORT, ASSIGN, CLASS_BEGIN, METHOD_BEGIN, ASSIGN, METHOD_END,
-                METHOD_BEGIN, ASYNC, GLOBAL, ASSIGN, WHILE_BEGIN, ASSIGN, IF_BEGIN, BREAK, IF_END, WHILE_END, ASSIGN, DEL, ASSERT, APPLY, ASSIGN,
-                LAMBDA, LAMBDA, METHOD_BEGIN, ASSIGN, METHOD_BEGIN, NONLOCAL, ASSIGN, RETURN, METHOD_END, RETURN, APPLY, METHOD_END, ASSIGN, APPLY,
-                FOR_BEGIN, APPLY, IF_BEGIN, APPLY, IF_BEGIN, CONTINUE, IF_END, IF_END, FOR_END, TRY_BEGIN, WITH_BEGIN, APPLY, ASSIGN, APPLY, WITH_END,
-                EXCEPT_BEGIN, PASS, EXCEPT_END, FINALLY_BEGIN, APPLY, FINALLY_END, TRY_END, MATCH_BEGIN, CASE, RETURN, CASE, RETURN, MATCH_END,
-                METHOD_END, CLASS_END, METHOD_BEGIN, ASYNC, ASSIGN, APPLY, AWAIT, AWAIT, APPLY, METHOD_END, IF_BEGIN, IMPORT, APPLY, APPLY, IF_END);
+        collector.testFile("BasicTest.py").testSourceCoverage().testTokenSequence(IMPORT, IMPORT, CLASS_BEGIN, METHOD_BEGIN, ASSIGN, ASSIGN, LIST,
+                METHOD_END, METHOD_BEGIN, ASSIGN, APPLY, RETURN, METHOD_END, METHOD_BEGIN, ASSIGN, APPLY, RETURN, METHOD_END, METHOD_BEGIN, IF_BEGIN,
+                RETURN, IF_END, RETURN, APPLY, APPLY, METHOD_END, CLASS_END, METHOD_BEGIN, IF_BEGIN, RETURN, DICTIONARY, IF_END, ASSIGN, ASSIGN,
+                FOR_BEGIN, ASSIGN, IF_BEGIN, ASSIGN, IF_END, FOR_END, RETURN, DICTIONARY, APPLY, METHOD_END, METHOD_BEGIN, IF_BEGIN, RETURN, LIST,
+                IF_BEGIN, RETURN, LIST, IF_END, IF_END, ASSIGN, LIST, WHILE_BEGIN, APPLY, APPLY, WHILE_END, RETURN, METHOD_END, METHOD_BEGIN,
+                TRY_BEGIN, APPLY, RETURN, EXCEPT_BEGIN, RETURN, EXCEPT_END, TRY_END, METHOD_END, METHOD_BEGIN, ASSIGN, APPLY, ASSIGN, APPLY, ASSIGN,
+                APPLY, ASSIGN, LIST, ASSIGN, APPLY, IF_BEGIN, APPLY, APPLY, IF_END, FOR_BEGIN, APPLY, IF_BEGIN, CONTINUE, IF_END, IF_BEGIN, BREAK,
+                IF_END, APPLY, FOR_END, IF_BEGIN, NAMED_EXPR, APPLY, APPLY, IF_END, ASSIGN, LIST, IF_BEGIN, NAMED_EXPR, APPLY, APPLY, IF_BEGIN, APPLY,
+                IF_END, IF_END, TRY_BEGIN, ASSIGN, APPLY, APPLY, EXCEPT_BEGIN, APPLY, EXCEPT_END, TRY_END, ASSIGN, LAMBDA, LAMBDA, ASSIGN, APPLY,
+                RETURN, APPLY, METHOD_END, IF_BEGIN, ASSIGN, APPLY, APPLY, IF_END);
 
         collector.testFile("AsyncTest.py").testSourceCoverage().testTokenSequence(METHOD_BEGIN, ASYNC, AWAIT, AWAIT, APPLY, YIELD, YIELD, METHOD_END,
                 METHOD_BEGIN, ASYNC, FOR_BEGIN, ASYNC, APPLY, AWAIT, AWAIT, APPLY, FOR_END, METHOD_END, METHOD_BEGIN, ASYNC, WITH_BEGIN, ASYNC, APPLY,
@@ -72,31 +74,31 @@ public class PythonLanguageTest extends LanguageModuleTest {
                 MATCH_END, METHOD_END);
 
         collector.testFile("ExpressionsTest.py").testSourceCoverage().testTokenSequence(METHOD_BEGIN, ASSIGN, LAMBDA, LAMBDA, ASSIGN, LAMBDA, LAMBDA,
-                ASSIGN, LAMBDA, LAMBDA, ASSIGN, ASSIGN, APPLY, APPLY, LAMBDA, LAMBDA, ASSIGN, APPLY, APPLY, LAMBDA, LAMBDA, RETURN, METHOD_END,
-                METHOD_BEGIN, ASSIGN, IF_BEGIN, NAMED_EXPR, APPLY, APPLY, IF_END, WHILE_BEGIN, NAMED_EXPR, APPLY, APPLY, IF_BEGIN, APPLY, BREAK,
+                ASSIGN, LAMBDA, LAMBDA, ASSIGN, LIST, ASSIGN, APPLY, APPLY, LAMBDA, LAMBDA, ASSIGN, APPLY, APPLY, LAMBDA, LAMBDA, RETURN, METHOD_END,
+                METHOD_BEGIN, ASSIGN, LIST, IF_BEGIN, NAMED_EXPR, APPLY, APPLY, IF_END, WHILE_BEGIN, NAMED_EXPR, APPLY, APPLY, IF_BEGIN, APPLY, BREAK,
                 IF_END, WHILE_END, ASSIGN, APPLY, NAMED_EXPR, IF_BEGIN, NAMED_EXPR, APPLY, RETURN, IF_END, METHOD_END, METHOD_BEGIN, ASSIGN, LAMBDA,
-                LAMBDA, IF_BEGIN, NAMED_EXPR, APPLY, ASSIGN, LAMBDA, LAMBDA, APPLY, RETURN, APPLY, IF_END, METHOD_END, IF_BEGIN, APPLY, APPLY, APPLY,
-                IF_END);
+                LAMBDA, IF_BEGIN, NAMED_EXPR, APPLY, LIST, ASSIGN, LAMBDA, LAMBDA, APPLY, RETURN, APPLY, IF_END, METHOD_END, IF_BEGIN, APPLY, APPLY,
+                APPLY, IF_END);
 
         collector.testFile("ControlFlowTest.py").testSourceCoverage().testTokenSequence(METHOD_BEGIN, ASSIGN, WHILE_BEGIN, ASSIGN, IF_BEGIN, BREAK,
-                IF_END, WHILE_END, ASSIGN, ASSIGN, WHILE_BEGIN, ASSIGN, IF_BEGIN, CONTINUE, IF_END, APPLY, IF_BEGIN, APPLY, BREAK, IF_END, WHILE_END,
-                RETURN, METHOD_END, METHOD_BEGIN, ASSIGN, FOR_BEGIN, APPLY, ASSIGN, ASSIGN, WHILE_BEGIN, ASSIGN, IF_BEGIN, CONTINUE, IF_END, IF_BEGIN,
-                BREAK, IF_END, APPLY, WHILE_END, APPLY, FOR_END, RETURN, METHOD_END, METHOD_BEGIN, ASSIGN, FOR_BEGIN, APPLY, IF_BEGIN, CONTINUE,
-                IF_END, ASSIGN, WHILE_BEGIN, ASSIGN, IF_BEGIN, CONTINUE, IF_END, IF_BEGIN, BREAK, IF_END, APPLY, WHILE_END, IF_BEGIN, BREAK, IF_END,
-                FOR_END, RETURN, METHOD_END, METHOD_BEGIN, ASSIGN, WHILE_BEGIN, ASSIGN, IF_BEGIN, BREAK, IF_END, IF_BEGIN, PASS, IF_END, WHILE_END,
-                METHOD_END, METHOD_BEGIN, FOR_BEGIN, APPLY, IF_BEGIN, BREAK, IF_END, IF_BEGIN, PASS, IF_END, FOR_END, METHOD_END, IF_BEGIN, APPLY,
-                APPLY, APPLY, APPLY, APPLY, IF_END);
+                IF_END, WHILE_END, ASSIGN, LIST, ASSIGN, WHILE_BEGIN, ASSIGN, IF_BEGIN, CONTINUE, IF_END, APPLY, IF_BEGIN, APPLY, BREAK, IF_END,
+                WHILE_END, RETURN, METHOD_END, METHOD_BEGIN, ASSIGN, LIST, FOR_BEGIN, APPLY, ASSIGN, LIST, ASSIGN, WHILE_BEGIN, ASSIGN, IF_BEGIN,
+                CONTINUE, IF_END, IF_BEGIN, BREAK, IF_END, APPLY, WHILE_END, APPLY, FOR_END, RETURN, METHOD_END, METHOD_BEGIN, ASSIGN, LIST,
+                FOR_BEGIN, APPLY, IF_BEGIN, CONTINUE, IF_END, ASSIGN, WHILE_BEGIN, ASSIGN, IF_BEGIN, CONTINUE, IF_END, IF_BEGIN, BREAK, IF_END, APPLY,
+                WHILE_END, IF_BEGIN, BREAK, IF_END, FOR_END, RETURN, METHOD_END, METHOD_BEGIN, ASSIGN, WHILE_BEGIN, ASSIGN, IF_BEGIN, BREAK, IF_END,
+                IF_BEGIN, PASS, IF_END, WHILE_END, METHOD_END, METHOD_BEGIN, FOR_BEGIN, APPLY, IF_BEGIN, BREAK, IF_END, IF_BEGIN, PASS, IF_END,
+                FOR_END, METHOD_END, IF_BEGIN, APPLY, APPLY, APPLY, APPLY, APPLY, IF_END);
 
         collector.testFile("ExceptionGroupTest.py")
                 // TODO: Update to Python 3.12 first .testSourceCoverage()
-                .testTokenSequence(METHOD_BEGIN, METHOD_BEGIN, ASSIGN, APPLY, APPLY, APPLY, APPLY, IF_BEGIN, RAISE, APPLY, IF_END, METHOD_END,
-                        TRY_BEGIN, APPLY, APPLY, APPLY, APPLY, TRY_END, METHOD_END, METHOD_BEGIN, METHOD_BEGIN, ASSIGN, APPLY, APPLY, ASSIGN, APPLY,
-                        TRY_BEGIN, RAISE, APPLY, EXCEPT_BEGIN, APPLY, RAISE, APPLY, EXCEPT_END, TRY_END, METHOD_END, TRY_BEGIN, APPLY, APPLY, APPLY,
-                        APPLY, APPLY, TRY_END, METHOD_END, IF_BEGIN, APPLY, APPLY, IF_END);
+                .testTokenSequence(METHOD_BEGIN, METHOD_BEGIN, ASSIGN, LIST, APPLY, APPLY, APPLY, APPLY, IF_BEGIN, RAISE, APPLY, IF_END, METHOD_END,
+                        TRY_BEGIN, APPLY, APPLY, APPLY, APPLY, TRY_END, METHOD_END, METHOD_BEGIN, METHOD_BEGIN, ASSIGN, LIST, APPLY, APPLY, ASSIGN,
+                        LIST, APPLY, TRY_BEGIN, RAISE, APPLY, EXCEPT_BEGIN, APPLY, RAISE, APPLY, EXCEPT_END, TRY_END, METHOD_END, TRY_BEGIN, APPLY,
+                        APPLY, APPLY, APPLY, APPLY, TRY_END, METHOD_END, IF_BEGIN, APPLY, APPLY, IF_END);
 
-        collector.testFile("TypeAliasTest.py").testSourceCoverage().testTokenSequence(IMPORT, TYPE_ALIAS, TYPE_ALIAS, METHOD_BEGIN, ASSIGN, ASSIGN,
-                METHOD_BEGIN, FOR_BEGIN, APPLY, FOR_END, METHOD_END, APPLY, RETURN, METHOD_END, METHOD_BEGIN, TYPE_ALIAS, TYPE_ALIAS, ASSIGN, ASSIGN,
-                LAMBDA, LAMBDA, APPLY, RETURN, METHOD_END, IF_BEGIN, APPLY, APPLY, IF_END);
+        collector.testFile("TypeAliasTest.py").testSourceCoverage().testTokenSequence(IMPORT, TYPE_ALIAS, TYPE_ALIAS, METHOD_BEGIN, ASSIGN, LIST,
+                ASSIGN, METHOD_BEGIN, FOR_BEGIN, APPLY, FOR_END, METHOD_END, APPLY, RETURN, METHOD_END, METHOD_BEGIN, TYPE_ALIAS, TYPE_ALIAS, LIST,
+                ASSIGN, LIST, LIST, LIST, ASSIGN, LAMBDA, LAMBDA, APPLY, RETURN, METHOD_END, IF_BEGIN, APPLY, APPLY, IF_END);
     }
 
     @Override
