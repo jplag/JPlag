957beaf60d7d719ca1e054b4152a7ec03fb637c7
heylissi <e2.hermann@gmail.com>
2025-08-15 04:27:27 +0200

    zwischenstand

---
+++ core/src/main/java/de/jplag/highlightextraction/SigmoidWeightingStrategy.java
@@ -0,0 +1,9 @@
+package de.jplag.highlightextraction;
+
+public class SigmoidWeightingStrategy extends AbstractSimilarityStrategy {
+    public double computeWeight(double minWeight, double maxWeight, double rarity) {
+        double norm = 1.0 / (1.0 + Math.exp(-10 * (rarity - 0.5)));
+        double normMax = 1.0 / (1.0 + Math.exp(-10 * (1 - 0.5)));
+        return minWeight + (maxWeight - minWeight) * (norm / normMax);
+    }
+}
