13c803b38a96acfd88deb550d8a0e9229e68f664
heylissi <e2.hermann@gmail.com>
2025-08-29 11:57:48 +0200

    java doc

--- core/src/main/java/de/jplag/highlightextraction/CompleteMatchesStrategy.java
+++ core/src/main/java/de/jplag/highlightextraction/CompleteMatchesStrategy.java
@@ -27,7 +27,7 @@ public class CompleteMatchesStrategy implements FrequencyStrategy {
     }
 
     /**
-     * Calculates the weight of a match.
+     * Calculates the weight of a match. According to the strategy.
      * @param match Considered match
      * @param frequencyMap Frequency map build with processMatchTokenTypes method
      * @param matchToken tokenType sequence of the match
