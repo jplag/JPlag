d53fa2c008906d8c0d05ff596cee505d1322d7b6
Alexander Vogt <alexander_vogt@outlook.de>
2025-09-03 14:23:14 +0200

    setup package

---
+++ report-viewer/cli-ui/src/main.ts
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+
+import App from './App.vue'
+
+import '@jplag/ui-components/style/style.css'
+
+const app = createApp(App)
+
+app.config.errorHandler = (err, vm, info) => {
+  console.error(err, info)
+  alert('An unhandled error occurred. Please check the console for more details.')
+}
+
+app.mount('#app')
