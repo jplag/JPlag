a13eb5dc7c4240d3b34426582377832b694821d0
Alexander Milster <alexander.milster@gmx.de>
2025-11-11 22:34:19 +0100

    Javadoc

--- language-testutils/src/test/java/de/jplag/testutils/AssertionUtils.java
+++ language-testutils/src/test/java/de/jplag/testutils/AssertionUtils.java
@@ -6,11 +6,11 @@ import java.util.function.Consumer;
 import org.junit.jupiter.api.Assertions;
 
 /**
- * Utility functions for assertions
+ * Utility functions for assertions.
  */
 public class AssertionUtils {
     /**
-     * Runs a function on all elements and fails if any of the functions fail, returning all errors to the user
+     * Runs a function on all elements and fails if any of the functions fail, returning all errors to the user.
      * @param items The list of items
      * @param check The check to perform
      * @param <T> The type of element
