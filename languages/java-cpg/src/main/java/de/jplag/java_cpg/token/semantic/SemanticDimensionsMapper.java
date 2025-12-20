d9daf20bbbc8f64063bceae5f5fae1c7576f0de0
uhvhe <uhvhe@student.kit.edu>
2025-12-20 21:13:56 +0100

    small refactoring, fixed spotless

--- languages/java-cpg/src/main/java/de/jplag/java_cpg/token/semantic/SemanticDimensionsMapper.java
+++ languages/java-cpg/src/main/java/de/jplag/java_cpg/token/semantic/SemanticDimensionsMapper.java
@@ -10,13 +10,11 @@ import de.fraunhofer.aisec.cpg.graph.statements.expressions.LambdaExpression;
 
 public final class SemanticDimensionsMapper {
     public static SemanticDimension mapNodeType(Node node) {
-        if (node instanceof MethodDeclaration){
+        if (node instanceof MethodDeclaration) {
             return SemanticDimension.METHOD_DECLARATION;
-        }
-        else if (node instanceof VariableDeclaration){
+        } else if (node instanceof VariableDeclaration) {
             return SemanticDimension.VARIABLE_DECLARATION;
-        }
-        else if (node instanceof BinaryOperator binaryOperator){
+        } else if (node instanceof BinaryOperator binaryOperator) {
             switch (binaryOperator.getOperatorCode()) {
                 case "&&", "||" -> {
                     return SemanticDimension.LOGICAL_EXPRESSION;
@@ -32,38 +30,28 @@ public final class SemanticDimensionsMapper {
                 }
                 default -> throw new IllegalStateException("Unexpected value: " + binaryOperator.getOperatorCode());
             }
-        }
-        else if (node instanceof ReturnStatement){
+        } else if (node instanceof ReturnStatement) {
             return SemanticDimension.RETURN_STATEMENT;
-        }
-        else if (node instanceof CaseStatement){
+        } else if (node instanceof CaseStatement) {
             return SemanticDimension.CASE;
-        }
-        else if (node instanceof SwitchStatement){
+        } else if (node instanceof SwitchStatement) {
             return SemanticDimension.SWITCH;
-        }
-        else if (node instanceof LambdaExpression){
+        } else if (node instanceof LambdaExpression) {
             return SemanticDimension.LAMBDA_EXPRESSION;
-        }
-        else if (node instanceof ConstructExpression){
+        } else if (node instanceof ConstructExpression) {
             return SemanticDimension.CLASS_OR_ARRAY_CREATOR;
-        }
-        else if (node instanceof IfStatement){
+        } else if (node instanceof IfStatement) {
             return SemanticDimension.IF;
-        }
-        else if (node instanceof AssertStatement){
+        } else if (node instanceof AssertStatement) {
             return SemanticDimension.ASSERT;
-        }
-        else if (node instanceof CatchClause){
+        } else if (node instanceof CatchClause) {
             return SemanticDimension.CATCH;
-        }
-        else if (node instanceof TryStatement){
-                return SemanticDimension.TRY;
-        }
-        else if (node instanceof ForEachStatement || node instanceof ForStatement || node instanceof WhileStatement || node instanceof DoStatement){
+        } else if (node instanceof TryStatement) {
+            return SemanticDimension.TRY;
+        } else if (node instanceof ForEachStatement || node instanceof ForStatement || node instanceof WhileStatement
+                || node instanceof DoStatement) {
             return SemanticDimension.LOOP;
-        }
-        else {
+        } else {
             return null;
         }
     }
