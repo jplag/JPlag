554116047dd2f0a378d4d58de9c0726418913700
uhvhe <uhvhe@student.kit.edu>
2025-12-20 21:00:03 +0100

    another minor bugfix

--- languages/java-cpg/src/main/java/de/jplag/java_cpg/token/semantic/SemanticDimensionsMapper.java
+++ languages/java-cpg/src/main/java/de/jplag/java_cpg/token/semantic/SemanticDimensionsMapper.java
@@ -11,13 +11,13 @@ import de.fraunhofer.aisec.cpg.graph.statements.expressions.LambdaExpression;
 public final class SemanticDimensionsMapper {
     public static SemanticDimension mapNodeType(Node node) {
         switch (node) {
-            case (node instanceof ReturnStatement): {
+            if (node instanceof ReturnStatement){
                 return SemanticDimension.METHOD_DECLARATION;
             }
-            case (node instanceof VariableDeclaration): {
+            else if (node instanceof VariableDeclaration){
                 return SemanticDimension.VARIABLE_DECLARATION;
             }
-            case (node instanceof BinaryOperator): {
+            else if (node instanceof BinaryOperator){
                 BinaryOperator binaryOperator = (BinaryOperator) node;
                 switch (binaryOperator.getOperatorCode()) {
                     case "&&", "||" -> {
@@ -35,37 +35,37 @@ public final class SemanticDimensionsMapper {
                     default -> throw new IllegalStateException("Unexpected value: " + binaryOperator.getOperatorCode());
                 }
             }
-            case (node instanceof ReturnStatement): {
+            else if (node instanceof ReturnStatement){
                 return SemanticDimension.RETURN_STATEMENT;
             }
-            case (node instanceof CaseStatement): {
+            else if (node instanceof CaseStatement){
                 return SemanticDimension.CASE;
             }
-            case (node instanceof SwitchStatement):{
+            else if (node instanceof SwitchStatement){
                 return SemanticDimension.SWITCH;
             }
-            case (node instanceof LambdaExpression): {
+            else if (node instanceof LambdaExpression){
                 return SemanticDimension.LAMBDA_EXPRESSION;
             }
-            case (node instanceof ConstructExpression): {
+            else if (node instanceof ConstructExpression){
                 return SemanticDimension.CLASS_OR_ARRAY_CREATOR;
             }
-            case (node instanceof IfStatement): {
+            else if (node instanceof IfStatement){
                 return SemanticDimension.IF;
             }
-            case (node instanceof AssertStatement): {
+            else if (node instanceof AssertStatement){
                 return SemanticDimension.ASSERT;
             }
-            case (node instanceof CatchClause): {
+            else if (node instanceof CatchClause){
                 return SemanticDimension.CATCH;
             }
-            case (node instanceof TryStatement): {
+            else if (node instanceof TryStatement){
                 return SemanticDimension.TRY;
             }
-            case (node instanceof ForEachStatement || node instanceof ForStatement || node instanceof WhileStatement || node instanceof DoStatement): {
+            else if (node instanceof ForEachStatement || node instanceof ForStatement || node instanceof WhileStatement || node instanceof DoStatement){
                 return SemanticDimension.LOOP;
             }
-            default -> {
+            else {
                 return null;
             }
         }
