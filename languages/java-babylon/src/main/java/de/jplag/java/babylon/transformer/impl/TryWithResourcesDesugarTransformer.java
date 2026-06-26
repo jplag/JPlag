7f661bdfec924f72ca91a5e8b05b6a0e95623b20
JFronny <git@akbvopenfl1.hopto.org>
2026-06-26 21:25:45 +0200

    style: align try-with-resources-desugar with other transformers

--- languages/java-babylon/src/main/java/de/jplag/java/babylon/transformer/impl/TryWithResourcesDesugarTransformer.java
+++ languages/java-babylon/src/main/java/de/jplag/java/babylon/transformer/impl/TryWithResourcesDesugarTransformer.java
@@ -7,12 +7,15 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.annotation.Nullable;
+
 import de.jplag.java.babylon.BabylonDSL;
 import de.jplag.java.babylon.transformer.SimpleTransformation;
 
 import com.google.auto.service.AutoService;
 import jdk.incubator.code.Block;
 import jdk.incubator.code.Body;
+import jdk.incubator.code.CodeContext;
 import jdk.incubator.code.CodeType;
 import jdk.incubator.code.Op;
 import jdk.incubator.code.Value;
@@ -60,85 +63,90 @@ public class TryWithResourcesDesugarTransformer implements SimpleTransformation,
             }
         }
 
-        Body.Builder ancestorBody = builder.parentBody();
-
-        Body.Builder tryBody = Body.Builder.of(ancestorBody, CoreType.FUNCTION_TYPE_VOID, builder.context());
-        {
-            Block.Builder bd = tryBody.entryBlock();
-            bd.context().mapBlock(tryOp.body().entryBlock(), bd);
-            bd.context().mapValues(tryOp.body().entryBlock().parameters(), liftedParameters);
-            for (int i = 0, resourcesSize = resources.size(); i < resourcesSize; i++) {
-                Body resource = resources.get(i);
-                Block resourceBlock = requireSingle(resource.blocks());
-                Iterator<Op> it = resourceBlock.children().reversed().iterator();
-                CoreOp.YieldOp resourceYield = (CoreOp.YieldOp) it.next();
-                CoreOp.VarOp varOp = (CoreOp.VarOp) it.next();
-
-                bd.context().mapBlock(resourceBlock, bd);
-                bd.context().mapValue(varOp.result(), liftedParameters.get(i));
-
-                List<Op> initializeResources = resourceBlock.children().subList(0, resourceBlock.children().size() - 2);
-                for (Op initOp : initializeResources) {
-                    bd.add(initOp);
-                }
-
-                Value initOperand = requireSingle(varOp.operands());
-                place(bd, varOp.location(), CoreOp.varStore(liftedParameters.get(i), bd.context().getValue(initOperand)));
-            }
-            copy(tryOp.body(), bd);
-        }
+        Body.Builder tryBody = buildTry(builder.parentBody(), builder.context(), liftedParameters, resources, tryOp.body());
 
         List<Body.Builder> catchBodies = new ArrayList<>();
         for (Body catchBody : tryOp.catchBodies()) {
-            CodeType t = catchBody.bodySignature().parameterTypes().getFirst();
-            Body.Builder body = Body.Builder.of(ancestorBody, CoreType.functionType(VOID, t), builder.context());
-            Block.Builder bd = body.entryBlock();
-
-            // the first entry loads the exception which is located at the try
-            // this corrects that
-            Iterator<Op> cbit = catchBody.entryBlock().ops().iterator();
-            if (cbit.hasNext()) {
-                if (!(cbit.next() instanceof CoreOp.VarOp))
-                    throw new IllegalStateException();
-                if (cbit.hasNext())
-                    bd.add(locationMarker(location(cbit.next())));
-            }
-
-            bd.context().mapValues(catchBody.entryBlock().parameters(), bd.parameters());
-            copy(catchBody, bd);
-            catchBodies.add(body);
+            catchBodies.add(buildCatch(builder.parentBody(), builder.context(), catchBody));
         }
 
-        Body.Builder finallyBody = Body.Builder.of(ancestorBody, CoreType.FUNCTION_TYPE_VOID, builder.context());
-        {
-            Block.Builder bd = finallyBody.entryBlock();
+        Body.Builder finallyBody = buildFinally(builder.parentBody(), builder.context(), liftedParameters, tryOp.finallyBody());
 
-            Op.Location finloc = location(tryOp.finallyBody());
-            bd.add(locationMarker(finloc));
-
-            for (Value var : liftedParameters) {
-                Value loadedVar = bd.add(CoreOp.varLoad(var));
-                Op.Location loc = location(var);
-                // technically not correct since this skips the extra exception handling
-                bd.add(JavaOp.if_(bd.parentBody()).if_(bd1 -> {
-                    Op.Result nil = place(bd1, loc, CoreOp.constant(J_L_OBJECT, null));
-                    place(bd1, loc, CoreOp.core_yield(place(bd1, loc, JavaOp.neq(loadedVar, nil))));
-                }).then(bd1 -> {
-                    place(bd1, loc, JavaOp.invoke(MethodRef.method(AutoCloseable.class, "close", void.class), loadedVar));
-                    place(bd1, loc, CoreOp.core_yield());
-                }).else_());
-            }
-
-            Body fb = tryOp.finallyBody();
-            if (fb != null) {
-                bd.context().mapBlock(fb.entryBlock(), bd);
-                copy(fb, bd);
-            }
-        }
-
-        JavaOp.TryOp newTry = JavaOp.try_(List.of(), tryBody, catchBodies, finallyBody);
-        newTry.setLocation(tryOp.location());
-        builder.add(newTry);
+        place(builder, tryOp.location(), JavaOp.try_(List.of(), tryBody, catchBodies, finallyBody));
         return builder;
+    }
+
+    private Body.Builder buildTry(Body.Builder connectedAncestorBody, CodeContext cc, List<Value> liftedParameters, List<Body> resources,
+            Body tryBody) {
+        Body.Builder bodyBuilder = Body.Builder.of(connectedAncestorBody, CoreType.FUNCTION_TYPE_VOID, cc);
+        bodyBuilder.entryBlock().context().mapBlock(tryBody.entryBlock(), bodyBuilder.entryBlock());
+        bodyBuilder.entryBlock().context().mapValues(tryBody.entryBlock().parameters(), liftedParameters);
+        for (int i = 0, resourcesSize = resources.size(); i < resourcesSize; i++) {
+            Body resource = resources.get(i);
+            Block resourceBlock = requireSingle(resource.blocks());
+            Iterator<Op> it = resourceBlock.children().reversed().iterator();
+            CoreOp.YieldOp resourceYield = (CoreOp.YieldOp) it.next();
+            CoreOp.VarOp varOp = (CoreOp.VarOp) it.next();
+
+            bodyBuilder.entryBlock().context().mapBlock(resourceBlock, bodyBuilder.entryBlock());
+            bodyBuilder.entryBlock().context().mapValue(varOp.result(), liftedParameters.get(i));
+
+            List<Op> initializeResources = resourceBlock.children().subList(0, resourceBlock.children().size() - 2);
+            for (Op initOp : initializeResources) {
+                bodyBuilder.entryBlock().add(initOp);
+            }
+
+            Value initOperand = requireSingle(varOp.operands());
+            place(bodyBuilder.entryBlock(), varOp.location(),
+                    CoreOp.varStore(liftedParameters.get(i), bodyBuilder.entryBlock().context().getValue(initOperand)));
+        }
+        copy(tryBody, bodyBuilder.entryBlock());
+        return bodyBuilder;
+    }
+
+    private Body.Builder buildCatch(Body.Builder connectedAncestorBody, CodeContext cc, Body catchBody) {
+        CodeType exceptionType = catchBody.bodySignature().parameterTypes().getFirst();
+        Body.Builder bodyBuilder = Body.Builder.of(connectedAncestorBody, CoreType.functionType(VOID, exceptionType), cc);
+
+        // the first entry loads the exception which is located at the try
+        // this corrects that
+        Iterator<Op> it = catchBody.entryBlock().ops().iterator();
+        if (it.hasNext()) {
+            if (!(it.next() instanceof CoreOp.VarOp))
+                throw new IllegalStateException();
+            if (it.hasNext())
+                bodyBuilder.entryBlock().add(locationMarker(location(it.next())));
+        }
+
+        bodyBuilder.entryBlock().context().mapValues(catchBody.entryBlock().parameters(), bodyBuilder.entryBlock().parameters());
+        copy(catchBody, bodyBuilder.entryBlock());
+        return bodyBuilder;
+    }
+
+    private Body.Builder buildFinally(Body.Builder connectedAncestorBody, CodeContext cc, List<Value> liftedParameters, @Nullable Body finallyBody) {
+        Body.Builder bodyBuilder = Body.Builder.of(connectedAncestorBody, CoreType.FUNCTION_TYPE_VOID, cc);
+
+        if (finallyBody != null) {
+            bodyBuilder.entryBlock().add(locationMarker(location(finallyBody)));
+        }
+
+        for (Value var : liftedParameters) {
+            Value loadedVar = bodyBuilder.entryBlock().add(CoreOp.varLoad(var));
+            Op.Location loc = location(var);
+            // technically not correct since this skips the extra exception handling
+            bodyBuilder.entryBlock().add(JavaOp.if_(bodyBuilder.entryBlock().parentBody()).if_(bd1 -> {
+                Op.Result nil = place(bd1, loc, CoreOp.constant(J_L_OBJECT, null));
+                place(bd1, loc, CoreOp.core_yield(place(bd1, loc, JavaOp.neq(loadedVar, nil))));
+            }).then(bd1 -> {
+                place(bd1, loc, JavaOp.invoke(MethodRef.method(AutoCloseable.class, "close", void.class), loadedVar));
+                place(bd1, loc, CoreOp.core_yield());
+            }).else_());
+        }
+
+        if (finallyBody != null) {
+            bodyBuilder.entryBlock().context().mapBlock(finallyBody.entryBlock(), bodyBuilder.entryBlock());
+            copy(finallyBody, bodyBuilder.entryBlock());
+        }
+        return bodyBuilder;
     }
 }
